f221478f97b443616bcfb29e5bd3fae5fe9a7e42
ls-2018 <38821215+ls-2018@users.noreply.github.com>
2019-03-07 18:25:54 +0800

    计算两个单链表所代表的的数之和

--- Python程序员面试算法宝典/链表/计算两个单链表所代表的的数之和.py
+++ Python程序员面试算法宝典/链表/计算两个单链表所代表的的数之和.py
@@ -18,8 +18,57 @@ class LNode():
         self.next = None
 
 
-def add():
-    pass
+def add(h1, h2):
+    if h1 is None or h1.next is None:
+        return
+    if h2 is None or h2.next is None:
+        return
+    c = 0  # 用来记录进位
+    sums = 0  # 用来记录两个结点相加的值
+    p1 = h1.next  # 用来遍历h1
+    p2 = h2.next  # 用来遍历h2
+    tmp = None  # 用来指向新创建的存储相加的和的结点
+    resultHead = LNode()  # 相加后链表头结点
+    resultHead.next = None
+    p = resultHead  # 用来指向链表resultHead最后一个结点
+    while p1 is not None and p2 is not None:
+        tmp = LNode()
+        tmp.next = None
+        sums = p1.data + p2.data + c
+        tmp.data = sums % 10  # 两节点相加和
+        c = sums // 10  # 进位
+        p.next = tmp
+        p = tmp
+        p1 = p1.next
+        p2 = p2.next
+    if p1 is None:
+        while p2 is not None:
+            tmp = LNode()
+            tmp.next = None
+            sums = p2.data + c
+            tmp.data = sums % 10
+            c = sums // 10
+            p.next = tmp
+            p = tmp
+            p2 = p2.next
+    if p2 is None:
+        while p1 is not None:
+            tmp = LNode()
+            tmp.next = None
+            sums = p1.data + c
+            tmp.data = sums % 10
+            c = sums // 10
+            p.next = tmp
+            p = tmp
+            p1 = p1.next
+
+    # 处理完之后还有进位
+    if c == 1:
+        tmp = LNode()
+        tmp.next = None
+        tmp.data = 1
+        p.next = tmp
+    return resultHead
 
 
 if __name__ == '__main__':
@@ -53,17 +102,19 @@ if __name__ == '__main__':
         cur = tmp
         i -= 1
 
-
     cur = head1.next
     while cur is not None:
-        print(cur.data)
-        cur=cur.next
+        print(cur.data, end='', sep='')
+        cur = cur.next
+    print()
     cur = head2.next
     while cur is not None:
-        print(cur.data)
+        print(cur.data, end='', sep='')
         cur = cur.next
-    addResult = add(head1,head2)
-    cur =addResult.next
+    print()
+    addResult = add(head1, head2)
+
+    cur = addResult.next
     while cur is not None:
-        print(cur.data)
-        cur=cur.next
+        print(cur.data, end='', sep='')
+        cur = cur.next
