1bc17c9f9ceec6eff58c3048e9c1b8c7a4d6e33c
ls-2018 <1214972346@qq.com>
2019-03-08 13:18:43 +0800

    １３、 如何展开链接列表.py

--- Python程序员面试算法宝典/链表/１３、 如何展开链接列表.py
+++ Python程序员面试算法宝典/链表/１３、 如何展开链接列表.py
@@ -55,7 +55,7 @@ class MergeList:
     def printList(self):
         tmp = self.head
         while tmp != None:
-            print(tmp.data, end='->', sep='')
+            print(tmp.data, end=',')
             tmp = tmp.down
 
 
