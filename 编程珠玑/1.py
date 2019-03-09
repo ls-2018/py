419cac9ad03e421b34b21a69a87809aa154c58f7
ls-2018 <38821215+ls-2018@users.noreply.github.com>
2019-03-09 18:32:34 +0800

    编程珠玑

---
+++ 编程珠玑/1.py
@@ -0,0 +1,14 @@
+'''
+一个最多包含 n 个正整数的文件，每个都小于 n,其中n=10,000,000.
+其中任何整数都不重复，
+
+输出：按升序排列的输出整数的列表
+'''
+import datetime
+start = datetime.datetime.today()
+f = open('无序.txt','w',encoding='utf8')
+for i in range(10000000):
+    f.write('%012d'%(10000000-i)+'\n')
+    # print('%08d'%i)
+f.close()
+print(datetime.datetime.today()-start)
