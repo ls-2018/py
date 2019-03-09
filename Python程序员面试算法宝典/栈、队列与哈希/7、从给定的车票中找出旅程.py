3d5a838ec3409a0cb0848be59beaef5e7da84abb
ls-2018 <38821215+ls-2018@users.noreply.github.com>
2019-03-09 19:00:59 +0800

    7、从给定的车票中找出旅程

--- Python程序员面试算法宝典/栈、队列与哈希/7、从给定的车票中找出旅程.py
+++ Python程序员面试算法宝典/栈、队列与哈希/7、从给定的车票中找出旅程.py
@@ -16,3 +16,34 @@
 
 """
 
+
+def printResult(inputs):
+    # 用来存储把inputs的键与值调换后的信息
+    reverseInput = dict()
+    for k, v in inputs.items():
+        reverseInput[v] = k
+    start = None
+
+    # 找到起点
+    for k, v in inputs.items():
+        if k not in reverseInput:
+            start = k
+            break
+    if start == None:
+        print('输入法不合理')
+        return
+    # 从起点出发按照顺序遍历路径
+    to=inputs.get(start)
+    while to != None:
+        print(start, '--->', to)
+        start = to
+        to = inputs.get(to)
+
+
+if __name__ == '__main__':
+    inputs = dict()
+    inputs['西安'] = '成都'
+    inputs['北京'] = '上海'
+    inputs['大连'] = '西安'
+    inputs['上海'] = '大连'
+    printResult(inputs)
