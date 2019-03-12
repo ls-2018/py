2f8980275459a2c7ddaee30e0531d9205024266c
ls-2018 <1214972346@qq.com>
2019-03-12 13:12:04 +0800

    9、求绝对值最小的数

--- Python程序员面试算法宝典/数组/11、找出数组中出现一次的数.py
+++ Python程序员面试算法宝典/数组/11、找出数组中出现一次的数.py
@@ -1,6 +1,10 @@
 """
 [1,2,4,5,6,4,2],唯一的数有1,5,6只要输出其中一个就行;前提条件不唯一的都是偶数次
 
+一个奇数个,分成两组,奇数组亦或得到的就是唯一的.
+
+分组依据:唯一的数的二进制位(0,1),肯定能分成两组,其他的数,也依据这个比特位分组
+
 """
 
 
@@ -17,11 +21,11 @@ def isOne(n, i):
 def findSingle(array):
     size = len(array)
     i = 0
-    while i < 32:
+    while i < 32:  # int类型在32位机器为32位
         result1 = result0 = count0 = count1 = 0
         j = 0
         while j < size:
-            if isOne(array[j], i):
+            if isOne(array[j], i):  # 判断这个数的第i为是否为1
                 result1 ^= array[j]  # 第i位为1的值亦或操作
                 count1 += 1  # 第i为为1的数字个数
             else:
@@ -29,17 +33,22 @@ def findSingle(array):
                 count0 += 1
             j += 1
         i += 1
-        if count1 % 2 == 1 and result0 != 0:
+
+        # 判断哪一个是奇数组,
+        if count1 % 2 == 1 and result0 != 0:  # 如果result0=0 ,不能区分这三个数字,不能将其分配到两个数组内;有可能三个都跑到一个数组内了,因此偶数组编变成了零
             return result1
         if count0 % 2 == 1 and result1 != 0:
             return result0
-    return -1
+        """
+        假如说,第一次判断第0为是否为1 时,都为0
+        ---->result1 = result0 = count0 = count1 = 0
+        """
 
 
 if __name__ == '__main__':
-    arr = [6, 5, 9, 4, 4, 3, 3]
+    arr = [6, 5, 9, 4, 4, 3, 3, ]
     result = findSingle(arr)
-    if result != -1:
+    if result:
         print(result)
     else:
         print('没找到')
