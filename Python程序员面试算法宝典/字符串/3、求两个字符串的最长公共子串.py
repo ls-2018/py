cb0fee55f455ebb85bf222a4587c03981c7f63a8
ls-2018 <38821215+ls-2018@users.noreply.github.com>
2019-03-13 18:48:00 +0800

    3、求两个字符串的最长公共子串

--- Python程序员面试算法宝典/字符串/3、求两个字符串的最长公共子串.py
+++ Python程序员面试算法宝典/字符串/3、求两个字符串的最长公共子串.py
@@ -1,19 +1,24 @@
+"""
+    首先定义二元函数f(i,j),表示分别以s1[i],s2[j]结尾的公共子串的长度，显然 f(0,j)=0(j>=0)
+    f(i,0)=0(i>=0),那么对于f(i+1,j+1)而言，则有如下两种取值
+    1、f(i+1,j+1) = 0,当str1[i+1]!=str2[j+1]
+    2、f(i+1,j+1)=f(i,j)+1,当str1[i+1]=str2[j+1]
+
+
+"""
+
+
 # 方法一:动态规划
-"""
-首先定义二元函数f(i,j),表示分别以s1[i],s2[j]结尾的公共子串的长度，显然 f(0,j)=0(j>=0)
-f(i,0)=0(i>=0),那么对于f(i+1,j+1)而言，则有如下两种取值
-1、f(i+1,j+1) = 0,当str1[i+1]!=str2[j+1]
-2、f(i+1,j+1)=f(i,j)+1,当str1[i+1]=str2[j+1]
-
-"""
-
-
 def getMaxSubStr(str1, str2):
     """
     获取两个字符串的最长子字符串
-    :param str1:
-    :param str2:
-    :return:
+                len1
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+len2    [0, 0, 0, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0]
+    缺点:额外开辟了m*n的空间
     """
     len1 = len(str1)
     len2 = len(str2)
@@ -55,14 +60,44 @@ def getMaxSubStr(str1, str2):
     return sb
 
 
-"""
-            len1
-        [0, 0, 0, 0], 
-        [0, 0, 0, 0], 
-len2    [0, 0, 0, 0], 
-        [0, 0, 0, 0], 
-        [0, 0, 0, 0]
- 
-"""
+# 方法二:滑动比较法
+def getMaxSubStr_2(s1, s2):
+    len1 = len(s1)
+    len2 = len(s2)
+    maxLen = 0
+    tmpMaxLen = 0
+    maxLenEnd1 = 0
+    sb = ''
+    i = 0
+    while i < len1 + len2:
+        s1begin = s2begin = 0
+        tmpMaxLen = 0
+        if i < len1:
+            s1begin = len1 - i
+        else:
+            s2begin = i - len1
+        j = 0
+        while (s1begin + j < len1) and (s2begin + j < len2):
+            if list(s1)[s1begin + j] == list(s2)[s2begin + j]:
+                tmpMaxLen += 1
+            else:
+                if tmpMaxLen > maxLen:
+                    maxLen = tmpMaxLen
+                    maxLenEnd1 = s1begin + j
+                else:
+                    tmpMaxLen = 0
+            j += 1
+        if tmpMaxLen > maxLen:
+            maxLen = tmpMaxLen
+            maxLenEnd1 = s1begin + j
+        i += 1
+    i = maxLenEnd1 - maxLen
+    while i < maxLenEnd1:
+        sb += list(s1)[i]
+        i += 1
+    return sb
+
+
 if __name__ == '__main__':
-    print(getMaxSubStr('aszzzzzzzzzzzzzzzdc', 'aszzzzzzzzzzzzzzzdczzzzzzzzzzzzzzzz'))
+    # print(getMaxSubStr('aszzzzzzzzzzzzzzzdc', 'aszzzzzzzzzzzzzzzdczzzzzzzzzzzzzzzz'))
+    print(getMaxSubStr_2('aszzzzzzzzzzzzzzzdc', 'aszzzzzzzzzzzzzzzdczzzzzzzzzzzzzzzz'))
