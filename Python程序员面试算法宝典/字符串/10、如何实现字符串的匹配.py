8d6c0d49127447f1f8bf93f74c5d3dc0af8d88d3
ls-2018 <1214972346@qq.com>
2019-03-14 09:32:47 +0800

    10、如何实现字符串的匹配

--- Python程序员面试算法宝典/字符串/10、如何实现字符串的匹配.py
+++ Python程序员面试算法宝典/字符串/10、如何实现字符串的匹配.py
@@ -9,3 +9,38 @@
 这种方法的时间复杂度为O(m*n),此外如果i>m-n,那么在主串中以Si为首的子串的长度必定小于模式串P的长度.
 
 """
+
+
+def match(s, p):
+    # 检查参数的合理性
+    if s is None or p is None:
+        print('参数不合理')
+        return -1
+    slen = len(s)
+    plen = len(p)
+    # p 肯定不是s的子串
+    if slen < plen:
+        return -1
+
+    i = j = 0
+    while i < slen and j < plen:
+        if list(s)[i] == list(p)[j]:
+            # 如果相同,那么继续比较后面的字符
+            i += 1
+            j += 1
+        else:
+            # 后退回去重新比较
+            i = i - j + 1
+            j = 0
+            if i > slen - plen:  # 保证主串待匹配的字符数大于等于模式串剩余的字符数
+                return -1
+    if j >= plen:
+        # 匹配成功
+        return i - plen
+    return -1
+
+
+if __name__ == '__main__':
+    s = 'xyzabcd'
+    p = 'abc'
+    print(match(s, p))
