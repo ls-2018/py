37326c4741685d62023733c426938e6d7f6e0b4d
ls-2018 <1214972346@qq.com>
2019-03-16 08:49:48 +0800

    2、判断一个数是否为2的n次方

---
+++ Python程序员面试算法宝典/数字/2、判断一个数是否为2的n次方.py
@@ -0,0 +1,24 @@
+# 构造法
+def isPower_1(n):
+    """时间复杂度O(logn)"""
+    if n < 1:
+        return False
+    i = 1
+    while i <= n:
+        if i == n:
+            return True
+        i <<= 1
+    return False
+
+
+# 与操作法
+def isPower_2(n):
+    """时间复杂度O(1)"""
+    """
+    如果一个数是2的n次方,那么对应的二进制位,只有1位是1,  因此可以转化为求是否只有1个1 ;
+    如果是,那么  n  与  n-1的每一位都不相同,
+    """
+    if n < 1:
+        return False
+    m = n & (n - 1)
+    return m == 0
