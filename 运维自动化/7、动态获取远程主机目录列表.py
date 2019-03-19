5e101f86d33c0c61125569ed54c723b787ae2a35
ls-2018 <38821215+ls-2018@users.noreply.github.com>
2019-03-19 12:00:14 +0800

    _

---
+++ 运维自动化/7、动态获取远程主机目录列表.py
@@ -0,0 +1,21 @@
+from fabric.api import *
+
+env.user = "root"
+env.hosts = ["192.168.165.45", "192.168.165.38"]
+env.password = "NFjd1234"
+
+@runs_once
+def input_raw():
+        return prompt("Pleace input dir_name:",default="/home")
+
+def worktask(dirname):
+        run("ls -l " + dirname)
+
+@task
+def go():
+        getdirname = input_raw()
+        worktask(getdirname)
+
+
+#  fab -f 7、动态获取远程主机目录列表.py go
+#  fab -f 7、动态获取远程主机目录列表.py local_task
