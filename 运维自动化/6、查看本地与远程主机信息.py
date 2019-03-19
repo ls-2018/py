544314128e2532f9359a8ec335bb2d38dda1cf80
ls-2018 <38821215+ls-2018@users.noreply.github.com>
2019-03-19 12:43:30 +0800

    6、查看本地与远程主机信息

--- 运维自动化/6、查看本地与远程主机信息.py
+++ 运维自动化/6、查看本地与远程主机信息.py
@@ -7,10 +7,12 @@ env.user = "root"
 env.hosts = ["192.168.165.45", "192.168.165.38"]
 env.password = "NFjd1234"
 
-@runs_once      #查看本地系统信息，当有多台主机时只运行一次
+
+@runs_once  # 查看本地系统信息，当有多台主机时只运行一次
 def local_task():  # 本地任务函数
     local("uname -a")
     local("df -h")
+
 
 def remote_task():
     with cd("/home"):  # with的作用是让后面的表达式语句集成当前状态，效果相对于"cd /home && ls -l"
