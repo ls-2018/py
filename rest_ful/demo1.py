3d9c6b52b3adf63fd91df108c07261385f0cdb87
ls-2018 <38821215+ls-2018@users.noreply.github.com>
2019-03-21 16:07:28 +0800

    _

--- rest_ful/demo1.py
+++ rest_ful/demo1.py
@@ -1,7 +1,8 @@
-from rest_framework.pagination import PageNumberPagination, LimitOffsetPagination,CursorPagination
+from rest_framework.pagination import PageNumberPagination, LimitOffsetPagination, CursorPagination
+
 
 class MyPagination(CursorPagination):
     # - 根据加密：http: // www.luffycity.com / api / v1 / student /?cursor = erd8
-    cursor_query_param = 'cursor'   #  根据什么参数取数据?cursor=1
+    cursor_query_param = 'cursor'  # 根据什么参数取数据?cursor=1
     ordering = '-created'
     page_size = 2
