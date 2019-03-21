2b70e8477218ed7688ce08e1babb10e70545151e
ls-2018 <38821215+ls-2018@users.noreply.github.com>
2019-03-21 16:37:28 +0800

    _

--- rest_ful/demo1.py
+++ rest_ful/demo1.py
@@ -6,3 +6,11 @@ class MyPagination(CursorPagination):
     cursor_query_param = 'cursor'  # 根据什么参数取数据?cursor=1
     ordering = '-created'
     page_size = 2
+
+from rest_framework.views import APIView
+
+# 'DEFAULT_PARSER_CLASSES': (
+#     'rest_framework.parsers.JSONParser',
+#     'rest_framework.parsers.FormParser',
+#     'rest_framework.parsers.MultiPartParser'
+# )
