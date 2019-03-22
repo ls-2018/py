fe83a93b0c755f177034a520df391281c7bc13a7
ls-2018 <38821215+ls-2018@users.noreply.github.com>
2019-03-22 09:28:33 +0800

    _

--- rest_ful/demo1.py
+++ rest_ful/demo1.py
@@ -23,3 +23,10 @@ from rest_framework.views import APIView
 # from . import models
 # for tables in models.__all__:
 #     admin.site.register(getattr(models,table))
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.fields import GenericForeignKey,GenericRelation
+
+# biao1
+# a= xxx()# 表的名字
+# b=xxx()   # 内部实例
+# c = GenericForeignKey('contenttype',b)
