5b9160a7372da7a6acefa6c139a880753061c320
ls-2018 <38821215+ls-2018@users.noreply.github.com>
2019-04-01 18:23:59 +0800

    sanic框架

---
+++ sanic框架/1、入门.py
@@ -0,0 +1,12 @@
+from sanic import Sanic
+
+from sanic.response import json
+
+app = Sanic()
+
+@app.route('/')
+async def test(request):
+    return json({'json':"hello"})
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0',port=8000)
