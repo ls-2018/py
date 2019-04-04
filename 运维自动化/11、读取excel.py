5c4d3a4292405fed67fe1180ae5a96216436199a
ls-2018 <1214972346@qq.com>
2019-04-04 12:46:24 +0800

    运维自动化

--- 运维自动化/11、读取excel.py
+++ 运维自动化/11、读取excel.py
@@ -48,20 +48,8 @@ for row_cell in wb1['B2':'G10']:
     for cell in row_cell:
         cell.alignment = Alignment(horizontal='center', vertical='center')  # right,left
 
-# ############# 设定单元格的边框、字体、颜色、大小和边框背景色 ###############
-from openpyxl.styles import NamedStyle, Font, Border, Side, PatternFill
-
-highlight = NamedStyle(name="highlight")
-highlight.font = Font(bold=True, size=20, color="ff0100")
-highlight.fill = PatternFill("solid", fgColor="DDDDDD")  # 背景填充
-bd = Side(style='thick', color="000000")
-highlight.border = Border(left=bd, top=bd, right=bd, bottom=bd)
-
-print(dir(wb1["A1"]))
-wb1["A1"].style = highlight
-
 # ######################## 设置行高和列宽 ####################################
-# 有时候数据太长显示不完，就需要拉长拉高单元格。
+# 有时候数据太长显示不完，就需要拉长拉高单元格
 # 第2行行高
 wb1.row_dimensions[2].height = 40
 # C列列宽
@@ -75,4 +63,46 @@ wb1.merge_cells('B1:G1')  # 合并一行中的几个单元格
 换句话说若合并前不是在左上角写入数据，合并后单元格中不会有数据。
 """
 # wb1.unmerge_cells('A1:C3')
+
+
+# ############# 设定单元格的边框、字体、颜色、大小和边框背景色 ###############
+from openpyxl.styles import NamedStyle, Font, Border, Side, PatternFill
+
+highlight = NamedStyle(name="highlight")
+highlight.font = Font(name='微软雅黑', size=11, bold=False, italic=False, vertAlign=None, underline='none', strike=False,
+                      color='FF000000')
+highlight.fill = PatternFill("solid", fgColor="DDDDDD")  # 背景填充
+bd = Side(style='thick', color="000000")
+highlight.border = Border(left=bd, top=bd, right=bd, bottom=bd)
+
+print(dir(wb1["A1"]))
+wb1["A1"].style = highlight
+# ############# 常用的样式和属性设置 ###############
+
+
+from openpyxl.styles import PatternFill, Border, Side, Alignment, Protection, Font
+
+fill = PatternFill(fill_type="solid", start_color='FFEEFFFF', end_color='FF001100')
+
+# 边框可以选择的值为：'hair', 'medium', 'dashDot', 'dotted', 'mediumDashDot', 'dashed', 'mediumDashed', 'mediumDashDotDot', 'dashDotDot', 'slantDashDot', 'double', 'thick', 'thin']
+# diagonal 表示对角线
+bd = Border(left=Side(border_style="thin", color='FF001000'),
+            right=Side(border_style="thin", color='FF110000'),
+            top=Side(border_style="thin", color='FF110000'),
+            bottom=Side(border_style="thin", color='FF110000'),
+            diagonal=Side(border_style=None, color='FF000000'),
+            diagonal_direction=0, outline=Side(border_style=None, color='FF000000'),
+            vertical=Side(border_style=None, color='FF000000'),
+            horizontal=Side(border_style=None, color='FF110000'))
+
+alignment = Alignment(horizontal='general', vertical='bottom', text_rotation=0, wrap_text=False, shrink_to_fit=False,
+                      indent=0)
+number_format = 'General'
+protection = Protection(locked=True, hidden=False)
+
+wb1["B5"].fill = fill
+wb1["B5"].border = bd
+wb1["B5"].alignment = alignment
+wb1["B5"].number_format = number_format
+
 wb.save('balances.xlsx')
