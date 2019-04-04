5778b7f09ab19bae79fd3e7c3054d9a39c454612
ls-2018 <1214972346@qq.com>
2019-04-04 11:36:37 +0800

    运维自动化

--- 运维自动化/10、操作excel.py
+++ 运维自动化/10、操作excel.py
@@ -1,12 +1,16 @@
 from openpyxl import Workbook
 
-wb = Workbook() # 默认有一个Sheet工作薄
+wb = Workbook()  # 默认有一个Sheet工作薄
 
 # 创作一个新的工作薄
 ws1 = wb.create_sheet('Mysheet')  # 在最后添加
 ws2 = wb.create_sheet('Mysheeta', 0)  # 在第一个添加
-#　修改工作薄的名称
+# 　修改工作薄的名称
 ws2.title = "New Title"
+
+# 写数据
+ws2["A4"] = 4
+ws2.cell(row=4, column=2, value=10)  # 按单元格来区分,从1开始
 
 
 # 保存
