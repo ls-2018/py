28d0f5a3cadfc2512b0ba69f5a2938468f0cc60a
ls-2018 <38821215+ls-2018@users.noreply.github.com>
2019-04-05 08:58:17 +0800

    Flask

--- Flask－/knowledge/1、.py
+++ Flask－/knowledge/1、.py
@@ -1,16 +1,20 @@
-from flask import Flask, Markup
+from flask import Flask, Markup,render_template
 
 app = Flask(__name__)
 
-
-@app.add_template_global()
+@app.template_global()
 def ab(a, b):
     return a + b
 
 
-@app.add_template_filter()
+@app.template_filter()
 def axb(a, b):
     return a * b  # 模板{{ 2| axb(3) }} 有些像偏函数
+
+
+@app.route('/', methods=[])
+def index():
+    return render_template('index.html')
 
 
 app.run(debug=True)
