109c81153333ecadaba7e94d834e44927307d1cc
ls-2018 <38821215+ls-2018@users.noreply.github.com>
2019-04-05 09:32:20 +0800

    Flask

--- Flask－/knowledge/1、.py
+++ Flask－/knowledge/1、.py
@@ -1,4 +1,4 @@
-from flask import Flask, Markup, render_template
+from flask import Flask, Markup, render_template,url_for
 
 app = Flask(__name__)
 
@@ -10,13 +10,24 @@ def ab(a, b):
 
 @app.template_filter()
 def axb(a, b):
-    print(a, b)# 2 3
+    print(a, b)  # 2 3
     return a * b  # 模板{{ 2| axb(3) }} 有些像偏函数
 
 
-@app.route('/')
+@app.route('/', endpoint='index')  # 用来反向生成url,绝对不能一样，
 def index():
+    print(url_for(index))
     return render_template('index.html')
+
+@app.before_request
+def auth():
+    """
+函数将在没有任何参数的情况下被调用。如果返回
+非空值，处理该值时将其视为返回值
+停止视图和进一步的请求处理。
+    :return:
+    """
+    pass
 
 
 app.run(debug=True)
