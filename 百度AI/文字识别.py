acac5dfd9f9a7619fb7bef9e2b069bdb76254df5
ls-2018 <38821215+ls-2018@users.noreply.github.com>
2019-04-07 12:00:27 +0800

    文字识别

--- 百度AI/文字识别.py
+++ 百度AI/文字识别.py
@@ -7,19 +7,17 @@ SECRET_KEY = 'i1WmYITKoFskWPp64Og55U7wqCaHxwGz'
 
 client = AipOcr(APP_ID, API_KEY, SECRET_KEY)
 
-
-
 """ 读取图片 """
-def get_file_content(filePath):
-    with open(filePath, 'rb') as fp:
-        return fp.read()
+# def get_file_content(filePath):
+#     with open(filePath, 'rb') as fp:
+#         return fp.read()
 
-image = get_file_content('test.jpg')
+# image = get_file_content('test.jpg')
 # {'log_id': 2794194094609589127, 'words_result_num': 1, 'words_result': [{'words': '5739'}]}
 
 """ 调用通用文字识别, 图片参数为本地图片 """
-res = client.basicGeneral(image)
-print(res)
+# res = client.basicGeneral(image)
+# print(res)
 # """ 如果有可选参数 """
 # options = {}
 # options["language_type"] = "CHN_ENG"
@@ -32,8 +30,9 @@ print(res)
 #
 # url = "http//www.x.com/sample.jpg"
 #
-# """ 调用通用文字识别, 图片参数为远程url图片 """
-# client.basicGeneralUrl(url)
+""" 调用通用文字识别, 图片参数为远程url图片 """
+# res = client.basicGeneralUrl(url='https://ps.ssl.qhimg.com/sdmt/432_162_100/t0171742c68d1bddc1f.webp')
+# print(res)
 #
 # """ 如果有可选参数 """
 # options = {}
@@ -41,7 +40,14 @@ print(res)
 # options["detect_direction"] = "true"
 # options["detect_language"] = "true"
 # options["probability"] = "true"
-#
+
 # """ 带参数调用通用文字识别, 图片参数为远程url图片 """
 # client.basicGeneralUrl(url, options)
 #
+
+url = "http://www.shuoiliu.com/media/123.jpg"
+
+""" 调用通用文字识别（含位置信息版）, 图片参数为远程url图片 """
+res = client.generalUrl(url)
+
+print(res)
