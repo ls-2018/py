2a179850555c695aa679f2b59084f9aa3204e7a3
ls-2018 <38821215+ls-2018@users.noreply.github.com>
2019-04-07 11:52:28 +0800

    文字识别

---
+++ 百度AI/文字识别.py
@@ -0,0 +1,47 @@
+from aip import AipOcr
+
+""" 你的 APPID AK SK """
+APP_ID = '15949600'
+API_KEY = 'k9tDMxR017I5cNVDrx0XOBsM'
+SECRET_KEY = 'i1WmYITKoFskWPp64Og55U7wqCaHxwGz'
+
+client = AipOcr(APP_ID, API_KEY, SECRET_KEY)
+
+
+
+""" 读取图片 """
+def get_file_content(filePath):
+    with open(filePath, 'rb') as fp:
+        return fp.read()
+
+image = get_file_content('test.jpg')
+# {'log_id': 2794194094609589127, 'words_result_num': 1, 'words_result': [{'words': '5739'}]}
+
+""" 调用通用文字识别, 图片参数为本地图片 """
+res = client.basicGeneral(image)
+print(res)
+# """ 如果有可选参数 """
+# options = {}
+# options["language_type"] = "CHN_ENG"
+# options["detect_direction"] = "true"
+# options["detect_language"] = "true"
+# options["probability"] = "true"
+#
+# """ 带参数调用通用文字识别, 图片参数为本地图片 """
+# client.basicGeneral(image, options)
+#
+# url = "http//www.x.com/sample.jpg"
+#
+# """ 调用通用文字识别, 图片参数为远程url图片 """
+# client.basicGeneralUrl(url)
+#
+# """ 如果有可选参数 """
+# options = {}
+# options["language_type"] = "CHN_ENG"
+# options["detect_direction"] = "true"
+# options["detect_language"] = "true"
+# options["probability"] = "true"
+#
+# """ 带参数调用通用文字识别, 图片参数为远程url图片 """
+# client.basicGeneralUrl(url, options)
+#
