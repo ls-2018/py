28c9f75f53a7fb0a270b48ae6a25ac499c0e12f4
ls-2018 <38821215+ls-2018@users.noreply.github.com>
2019-04-10 12:47:42 +0800

    pymongo

--- Mongo/connection.py
+++ Mongo/connection.py
@@ -22,22 +22,26 @@ print(MONGO.userinfo.find({"$or": [{'name': 'dwb'}, {'age': 12}]}))
 
 ###############插入  #############
 res = MONGO.userinfo.insert_one({'name': 'bs', 'age': 13})
-print(res,res.inserted_id)  # 不用commit
+print(res, res.inserted_id)  # 不用commit
 # <pymongo.cursor.Cursor object at 0x000001F26AC87630>
 # <pymongo.results.InsertOneResult object at 0x000001F26AC6B888> 5cad733cafd45c3ccc94404a
 
-res = MONGO.userinfo.insert_many([{},])
-print(res,res.inserted_ids)  # 不用commit  ,不再是字符串，而是对象
-
-
+res = MONGO.userinfo.insert_many([{}, ])
+print(res, res.inserted_ids)  # 不用commit  ,不再是字符串，而是对象
 
 #################################修改
-res = MONGO.userinfo.update_one({},{})    # 返回UpdateResult对象
-print(res.raw_result)   # 影响的行数
-
+res = MONGO.userinfo.update_one({}, {})  # 返回UpdateResult对象
+print(res.raw_result)  # 影响的行数
 
 # #############################删除
 # MONGO.userinfo.delete_one({})
 # MONGO.userinfo.delete_many({})    # 返回DeleteResult对象
 
 
+#
+
+list(MONGO.userinfo.find({}).limit(5))
+list(MONGO.userinfo.find({}).limit(5).skip(5))# 跳过5条取5条
+list(MONGO.userinfo.find({}).sort('age',pymongo.DESCENDING))# 倒序
+list(MONGO.userinfo.find({}).sort('age',pymongo.DESCENDING).skip(5).limit(2))# 倒序
+# 先按age降序排列，在跳过5条取2条
