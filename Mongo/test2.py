d84069a24497c4640987d317d2131d2213ff608a
ls-2018 <38821215+ls-2018@users.noreply.github.com>
2019-04-11 19:36:17 +0800

    mongo

---
+++ Mongo/test2.py
@@ -0,0 +1,9 @@
+import pymongo
+from bson import ObjectId
+
+mongo_client = pymongo.MongoClient(host="127.0.0.1",port=27017)
+MONGO = mongo_client["test"]
+
+# 查询数据
+res = list(MONGO.userinfo.find({ }))
+print(res)
