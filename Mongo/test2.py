7b2fbe0caef53e075ea84824b2a8b4ceec1a4fab
ls-2018 <38821215+ls-2018@users.noreply.github.com>
2019-04-11 19:44:46 +0800

    mongo

--- Mongo/test2.py
+++ Mongo/test2.py
@@ -1,14 +1,9 @@
 import pymongo
 from bson import ObjectId
 
-mongo_client = pymongo.MongoClient(host="127.0.0.1",port=27017)
+mongo_client = pymongo.MongoClient(host="127.0.0.1", port=27017)
 MONGO = mongo_client["test"]
 
 # 查询数据
-res = list(MONGO.userinfo.find({ }))
+res = list(MONGO.userinfo.find({}))
 print(res)
-
-
-
-
-
