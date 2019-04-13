99205025d85d0fca7dd6ff5e40cd123cdfba6f8c
ls-2018 <1214972346@qq.com>
2019-04-13 09:34:14 +0800

    智能玩具

--- html5plus/KingEight/settings.py
+++
@@ -1,5 +0,0 @@
-import pymongo
-
-mongo_client = pymongo.MongoClient(host='127.0.0.1', port=27017)
-MONGO=mongo_client['KingEight']
-XPP_URL = 'https://m.ximalaya.com/tracks/%s.json'
--- html5plus/test.py
+++
@@ -1,2 +0,0 @@
-demo = {}
-print(sum(demo.values()))
