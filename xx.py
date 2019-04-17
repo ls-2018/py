b8651016ce1eda8553e9f4ab60065abcedd1da7a
ls-2018 <38821215+ls-2018@users.noreply.github.com>
2019-04-17 21:56:48 +0800

    5、分布数据可视化-散点图

--- xx.py
+++ xx.py
@@ -1,30 +1,5 @@
-# by luffycity.com
-from scrapy import signals
-from scrapy.xlib.pydispatch import dispatcher
-from scrapy import signals
-
-
-def func():
-    pass
-
-
-dispatcher.connect(func, signals.spider_closed)
-
-# class MyExtend(object):
-#     def __init__(self):
-#         pass
-#
-#     @classmethod
-#     def from_crawler(cls, crawler):
-#         self = cls()
-#         crawler.signals.connect(self.x1, signal=signals.spider_opened)
-#         crawler.signals.connect(self.x2, signal=signals.spider_closed)
-#         return self
-#
-#     def x1(self, spider):
-#         print('open')
-#
-
-
-#     def x2(self, spider):
-#         print('close')
+import seaborn as sns
+import warnings
+warnings.filterwarnings("ignore")
+tips = sns.load_dataset('tips')
+print(tips.head())
