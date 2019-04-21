c17422edb33935bd4cd7a4697eb915faf276c85d
ls-2018 <38821215+ls-2018@users.noreply.github.com>
2019-04-21 15:23:31 +0800

    简易回测框架

--- 数据分析/简易回测框架.py
+++ 数据分析/简易回测框架.py
@@ -2,7 +2,12 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import tushare
+import datetime
+import dateutil
 
+CASH = 10000
+START_DATE = '2016-01-01'
+END_DATE = '2017-12-31'
 # trade_cal = tushare.trade_cal()
 # trade_cal.to_csv('trade_cal.csv')
 trade_cal = pd.read_csv('trade_cal.csv')
@@ -18,9 +23,13 @@ class Context:
         self.date_range = trade_cal[(trade_cal['isOpen'] == 1) &
                                     (trade_cal['calendarDate'] >= start_date) &
                                     (trade_cal['calendarDate'] <= end_date)]
+        # self.dt = datetime.datetime.strptime('',start_date)
+        self.dt = dateutil.parser.parse(start_date)
 
 
-# context = Context(1000, '2016-01-01', '2017-01-01')
+context = Context(CASH, START_DATE, END_DATE)
+
+
 # print(context.date_range)
 
 class G:
@@ -28,3 +37,21 @@ class G:
 
 
 g = G()
+
+
+# 获取历史数据
+def attribute_history(security, count, fields=('open', 'close', 'high', 'low', 'volume')):
+    end_date = (context.dt - datetime.timedelta(days=1)).strftime('%Y-%m-%d')
+    start_date = trade_cal[(trade_cal['isOpen'] == 1) & (trade_cal['calendarDate'] <= end_date)][-count:].iloc[0, :][
+        'calendarDate']
+    print(start_date, end_date)
+    return atttibute_daterange_history(security, start_date, end_date)
+
+
+def atttibute_daterange_history(security, start_date, end_date, fields=('open', 'close', 'high', 'low', 'volume')):
+    try:
+        f = open(security + '.csv')
+        df = pd.read_csv(f, index_col='date', parse_dates=['date']).loc[start_date:end_date, :]
+    except:
+        df = tushare.get_k_data(security, start_date, end_date)
+    return df[list(fields)]
