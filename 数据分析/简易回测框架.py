4596fbabd4f4871b4f3f943b336ae3c0c2a4ca51
ls-2018 <38821215+ls-2018@users.noreply.github.com>
2019-04-21 20:13:21 +0800

    简易回测框架

--- 数据分析/简易回测框架.py
+++ 数据分析/简易回测框架.py
@@ -24,13 +24,16 @@ class Context:
                                     (trade_cal['calendarDate'] >= start_date) &
                                     (trade_cal['calendarDate'] <= end_date)]
         # self.dt = datetime.datetime.strptime('',start_date)
-        self.dt = dateutil.parser.parse(start_date)  # ToDo: start_date 后一个交易日
+        # self.dt = dateutil.parser.parse(start_date)  # ToDo: start_date 后一个交易日
+        self.dt = None
 
 
 context = Context(CASH, START_DATE, END_DATE)
 
 
-# print(context.date_range)
+def set_benckmark(security):  # 只支持一只股票作为基准
+    context.benchmark = security
+
 
 class G:
     pass
@@ -72,9 +75,6 @@ def get_today_data(security):
     return data
 
 
-print(get_today_data('601318'))
-
-
 def __order(today_data, security, amount):
     p = today_data['close']
     if len(today_data) == 0:
@@ -84,14 +84,14 @@ def __order(today_data, security, amount):
         amount = context.cash // p
         print('现金不足,已调整为：%d' % amount)
     if amount % 100 != 0:
-        if amount != -context.positions.get(security,0):
+        if amount != -context.positions.get(security, 0):
             # 2345
             amount = amount // 100 * 100
             print('不是100的倍数，以调整为%d' % amount)
     # 300   -200
-    if context.positions.get(security,0) < -amount:
+    if context.positions.get(security, 0) < -amount:
         # 卖超过拥有的量
-        amount = -context.positions.get(security,0)
+        amount = -context.positions.get(security, 0)
         print('卖出股票不能超过持仓数，已调整为%d' % amount)
 
     context.positions[security] = context.positions.get(security, 0) + amount
@@ -102,38 +102,79 @@ def __order(today_data, security, amount):
     if context.positions[security] == 0:
         del context.positions[security]
 
+
 # __order(get_today_data('601318'),'601318',1000000)
 # print(context.positions)
 
 
-def order(security,amount):
+def order(security, amount):
     today_data = get_today_data(security)
-    __order(today_data,security,amount)
+    __order(today_data, security, amount)
 
-def order_target(security,amount):
-    if amount<0:
+
+def order_target(security, amount):
+    if amount < 0:
         print('数量不能为负数，已调整为0')
         amount = 0
     today_data = get_today_data(security)
-    hold_amount = context.positions.get(security,0)
-    delta_amount = amount -hold_amount # 要卖的股票数
-    __order(today_data,security,delta_amount)  # ToDo : T+1  closeabel total
+    hold_amount = context.positions.get(security, 0)
+    delta_amount = amount - hold_amount  # 要卖的股票数
+    __order(today_data, security, delta_amount)  # ToDo : T+1  closeabel total
 
-def order_value(security,value):
-    today_data = get_today_data(security)
-    amount = value//today_data['open']
-    __order(today_data,security,amount)
 
-def order_target_value(security,value):
+def order_value(security, value):
     today_data = get_today_data(security)
-    if value <0:
+    amount = value // today_data['open']
+    __order(today_data, security, amount)
+
+
+def order_target_value(security, value):
+    today_data = get_today_data(security)
+    if value < 0:
         print('价格不能为负，以调整为0')
-        value=0
-    hold_value = context.positions.get(security,0)*today_data['open']
+        value = 0
+    hold_value = context.positions.get(security, 0) * today_data['open']
 
-    delta_value = value-hold_value# 买卖的价格
-    order_value(security,delta_value)
+    delta_value = value - hold_value  # 买卖的价格
+    order_value(security, delta_value)
 
-order('601318',100)
-order_value('601318',30000)
-print(context.positions)
+
+# order('601318',100)
+# order_value('601318',30000)
+# print(context.positions)
+
+
+def run():
+    plt_df = pd.DataFrame(index=pd.to_datetime(context.date_range), columns=['value'])
+    init_value = context.cash
+
+    initialize(context)
+    last_prize = {}
+    for dt in context.date_range:
+        context.dt = dateutil.parser.parse(dt)
+        handle_data(context)
+        value = context.cash
+        for stock in context.positions:
+            today_data = get_today_data(stock)
+            # 考虑停牌的情况
+            if len(today_data) == 0:
+                p = last_prize[stock]
+            else:
+                p = today_data['open']
+                last_prize[stock] = p
+            value += p * context.positions[stock]
+        plt_df.loc[dt, 'value'] = value
+    plt_df['ratio'] = (plt_df['value'] - init_value) / init_value
+    print(plt_df)
+
+
+def initialize(context):
+    set_benckmark('601318')
+
+
+def handle_data(context):
+    order('601318', 100)
+
+
+if __name__ == '__main__':
+    run()
