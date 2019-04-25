e45a22386097335a28659e324b0fffc2258777a2
ls-2018 <38821215+ls-2018@users.noreply.github.com>
2019-04-25 15:51:33 +0800

    wanjie

---
+++ 爬虫/utils/wanjie.py
@@ -0,0 +1,50 @@
+from urllib import request
+import os
+from selenium import webdriver
+from multiprocessing.dummy import Pool as ThreadPool
+
+chorme_opt = webdriver.ChromeOptions()
+prefs = {"profile.managed_default_content_settings.images": 2}
+chorme_opt.add_experimental_option("prefs", prefs)
+
+bro = webdriver.Chrome(executable_path=r"./chromedriver.exe", chrome_options=chorme_opt)
+bro.get("http://www.gugu5.com/o/wanjiexianzong/")
+
+a_list = bro.find_elements_by_xpath('//*[@id="play_0"]/ul/li/a')
+pre_link = []
+img_link = []
+for a in a_list:
+    href = a.get_attribute('href')
+    title = a.get_attribute('title')
+    pre_link.append((href, title))
+
+
+def _get_img_1(demo):
+    href, title = demo
+    if os.path.exists(f"./wanjie/{title}"):
+        pass
+    else:
+        os.mkdir(f'./wanjie/{title}')
+    bro.get(href)
+    num = bro.find_elements_by_xpath('/html/body/div[8]/select/option').__len__()
+    for i in range(1, num):
+        href = bro.find_element_by_xpath('//*[@id="qTcms_pic"]').get_attribute('href')
+        img_link.append((title, href, i))
+        bro.find_element_by_xpath('/html/body/div[11]/a[3]').click()
+
+
+# /html/body/div[8]/select/option[1]
+def _get_img_2(title, url, num):
+    print(title, num, url)
+    request.urlretrieve(url, f'./wanjie/{title}/{num}.png')
+
+
+if __name__ == '__main__':
+    pool = ThreadPool(16)  # 线程
+
+    pool.map(_get_img_1, pre_link)
+    pool.join()
+    print('----')
+    pool.map(_get_img_2, img_link)
+    pool.close()
+    pool.join()
