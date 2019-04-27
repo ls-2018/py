049f35d8902eb89ab7264360b018db24790e93c7
ls-2018 <38821215+ls-2018@users.noreply.github.com>
2019-04-27 18:58:46 +0800

    wanjie

--- 爬虫/utils/wanjie.py
+++ 爬虫/utils/wanjie.py
@@ -33,6 +33,8 @@ def _get_img_1():
             btn = bro.find_element_by_xpath('//*[@id="qTcms_pic"]')
             btn.click()
         # /html/body/div[8]/select/option[1]
+
+
 def _get_img_2(title, src, num):
     print(title, num, src)
     request.urlretrieve(src, f'./wanjie/{title}/{num}.png')
