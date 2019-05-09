ac9c98355d0aef73474cc5b7f4c49b2a3a4f2404
ls-2018 <38821215+ls-2018@users.noreply.github.com>
2019-05-09 11:04:55 +0800

    MadKing

--- 运维自动化/MadKing/MadKingClient/plugins/plugin_api.py
+++ 运维自动化/MadKing/MadKingClient/plugins/plugin_api.py
@@ -1,10 +1,8 @@
 # _*_coding:utf-8_*_
 
-
 def LinuxSysInfo():
     from plugins.linux import sysinfo
     return sysinfo.collect()
-
 
 def WindowsSysInfo():
     from plugins.windows import sysinfo as win_sysinfo
