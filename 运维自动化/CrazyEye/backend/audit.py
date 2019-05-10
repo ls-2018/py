3c7b1ca879c31dcf32dd45b6f0fb11814db4a679
ls-2018 <38821215+ls-2018@users.noreply.github.com>
2019-05-10 12:38:57 +0800

    CrazyEye

--- 运维自动化/CrazyEye/backend/audit.py
+++ 运维自动化/CrazyEye/backend/audit.py
@@ -1,4 +1,4 @@
-#_*_coding:utf-8_*_
+# _*_coding:utf-8_*_
 
 import re
 
@@ -6,43 +6,41 @@ import re
 class AuditLogHandler(object):
     '''分析audit log日志'''
 
-    def __init__(self,log_file):
+    def __init__(self, log_file):
         self.log_file_obj = self._get_file(log_file)
 
-
-    def _get_file(self,log_file):
+    def _get_file(self, log_file):
 
         return open(log_file)
 
     def parse(self):
         cmd_list = []
         cmd_str = ''
-        catch_write5_flag = False #for tab complication
+        catch_write5_flag = False  # for tab complication
         for line in self.log_file_obj:
-            #print(line.split())
+            # print(line.split())
             line = line.split()
             try:
-                pid,time_clock,io_call,char = line[0:4]
+                pid, time_clock, io_call, char = line[0:4]
                 if io_call.startswith('read(4'):
-                    if char == '"\\177",':#回退
+                    if char == '"\\177",':  # 回退
                         char = '[1<-del]'
-                    if char == '"\\33OB",': #vim中下箭头
+                    if char == '"\\33OB",':  # vim中下箭头
                         char = '[down 1]'
-                    if char == '"\\33OA",': #vim中下箭头
+                    if char == '"\\33OA",':  # vim中下箭头
                         char = '[up 1]'
-                    if char == '"\\33OC",': #vim中右移
+                    if char == '"\\33OC",':  # vim中右移
                         char = '[->1]'
-                    if char == '"\\33OD",': #vim中左移
+                    if char == '"\\33OD",':  # vim中左移
                         char = '[1<-]'
-                    if char == '"\33[2;2R",': #进入vim模式
+                    if char == '"\33[2;2R",':  # 进入vim模式
                         continue
                     if char == '"\\33[>1;95;0c",':  # 进入vim模式
                         char = '[----enter vim mode-----]'
 
-
-                    if char == '"\\33[A",': #命令行向上箭头
+                    if char == '"\\33[A",':  # 命令行向上箭头
                         char = '[up 1]'
-                        catch_write5_flag = True #取到向上按键拿到的历史命令
+                        catch_write5_flag = True  # 取到向上按键拿到的历史命令
                     if char == '"\\33[B",':  # 命令行向上箭头
                         char = '[down 1]'
                         catch_write5_flag = True  # 取到向下按键拿到的历史命令
@@ -56,25 +54,27 @@ class AuditLogHandler(object):
                         catch_write5_flag = True
                         continue
                     if char == '"\\r",':
-                        cmd_list.append([time_clock,cmd_str])
+                        cmd_list.append([time_clock, cmd_str])
                         cmd_str = ''  # 重置
-                    if char == '"':#space
+                    if char == '"':  # space
                         cmd_str += ' '
 
-                if catch_write5_flag: #to catch tab completion
+                if catch_write5_flag:  # to catch tab completion
                     if io_call.startswith('write(5'):
-                        if io_call == '"\7",': #空键，不是空格，是回退不了就是这个键
+                        if io_call == '"\7",':  # 空键，不是空格，是回退不了就是这个键
                             pass
                         else:
                             cmd_str += char.strip('"",')
                         catch_write5_flag = False
             except ValueError as e:
-                print("\033[031;1mSession log record err,please contact your IT admin,\033[0m",e)
+                print("\033[031;1mSession log record err,please contact your IT admin,\033[0m", e)
 
-        #print(cmd_list)
+        # print(cmd_list)
         # for cmd in cmd_list:
         #     print(cmd)
         return cmd_list
+
+
 if __name__ == "__main__":
     parser = AuditLogHandler('tmp/ssh_log2_4')
     parser.parse()
