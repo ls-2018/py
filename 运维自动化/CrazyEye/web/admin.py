d1a9a0d0307e8eee225ac301ac1ad91dcabc17a2
ls-2018 <38821215+ls-2018@users.noreply.github.com>
2019-05-10 13:09:29 +0800

    CrazyEye

--- 运维自动化/CrazyEye/web/admin.py
+++ 运维自动化/CrazyEye/web/admin.py
@@ -1,12 +1,11 @@
-#_*_coding:utf-8_*_
+# _*_coding:utf-8_*_
 from django.contrib import admin
 from django import forms
 from django.contrib.auth.admin import UserAdmin
 
-
 from web import models
 
-from django.shortcuts import render_to_response,render,HttpResponse
+from django.shortcuts import render_to_response, render, HttpResponse
 
 from django.contrib.admin.views.decorators import staff_member_required
 
@@ -17,7 +16,8 @@ from django.conf.urls import include, url
 from django.contrib.auth.admin import UserAdmin
 from django.contrib.auth.forms import ReadOnlyPasswordHashField
 
-from django.contrib.auth import  forms as auth_form
+from django.contrib.auth import forms as auth_form
+
 
 class UserCreationForm(forms.ModelForm):
     """A form for creating new users. Includes all the required
@@ -27,7 +27,7 @@ class UserCreationForm(forms.ModelForm):
 
     class Meta:
         model = models.UserProfile
-        fields = ('email','name')
+        fields = ('email', 'name')
 
     def clean_password2(self):
         # Check that the two password entries match
@@ -52,13 +52,13 @@ class UserChangeForm(forms.ModelForm):
     password hash display field.
     """
     password = ReadOnlyPasswordHashField(label="Password",
-        help_text=("Raw passwords are not stored, so there is no way to see "
-                    "this user's password, but you can change the password "
-                    "using <a href=\"password/\">this form</a>."))
+                                         help_text=("Raw passwords are not stored, so there is no way to see "
+                                                    "this user's password, but you can change the password "
+                                                    "using <a href=\"password/\">this form</a>."))
 
     class Meta:
         model = models.UserProfile
-        fields = ('email','password','is_active', 'is_admin')
+        fields = ('email', 'password', 'is_active', 'is_admin')
 
     def clean_password(self):
         # Regardless of what the user provides, return the initial value.
@@ -75,33 +75,33 @@ class UserProfileAdmin(UserAdmin):
     # The fields to be used in displaying the User model.
     # These override the definitions on the base UserAdmin
     # that reference specific fields on auth.User.
-    list_display = ('id','email','is_admin','is_active')
+    list_display = ('id', 'email', 'is_admin', 'is_active')
     list_filter = ('is_admin',)
     fieldsets = (
-        (None, {'fields': ('email','name', 'password')}),
-        ('Personal info', {'fields': ('department','memo')}),
-        #('API TOKEN info', {'fields': ('token',)}),
-        ('Permissions', {'fields': ('is_active','is_staff','is_admin')}),
-        ('有权限操作的主机或主机组', {'fields': ('bind_hosts','host_groups')}),
-        ('用户其它权限', {'fields': ('user_permissions','groups')}),
-        ('账户有效期', {'fields': ('valid_begin_time','valid_end_time')}),
+        (None, {'fields': ('email', 'name', 'password')}),
+        ('Personal info', {'fields': ('department', 'memo')}),
+        # ('API TOKEN info', {'fields': ('token',)}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_admin')}),
+        ('有权限操作的主机或主机组', {'fields': ('bind_hosts', 'host_groups')}),
+        ('用户其它权限', {'fields': ('user_permissions', 'groups')}),
+        ('账户有效期', {'fields': ('valid_begin_time', 'valid_end_time')}),
     )
     # add_fieldsets is not a standard ModelAdmin attribute. UserAdmin
     # overrides get_fieldsets to use this attribute when creating a user.
     add_fieldsets = (
         (None, {
             'classes': ('wide',),
-            'fields': ('email',  'password1', 'password2','is_active','is_admin')}
-        ),
+            'fields': ('email', 'password1', 'password2', 'is_active', 'is_admin')}
+         ),
     )
     search_fields = ('email',)
     ordering = ('email',)
-    filter_horizontal = ('bind_hosts','user_permissions','groups')
+    filter_horizontal = ('bind_hosts', 'user_permissions', 'groups')
 
 
 class HostAdmin(admin.ModelAdmin):
-    search_fields = ('hostname','ip_addr')
-    list_display = ('hostname','ip_addr','port','system_type','enabled')
+    search_fields = ('hostname', 'ip_addr')
+    list_display = ('hostname', 'ip_addr', 'port', 'system_type', 'enabled')
 
 
 # class BindHostInline(admin.TabularInline):
@@ -114,12 +114,13 @@ class HostAdmin(admin.ModelAdmin):
 
 
 class HostUserAdmin(admin.ModelAdmin):
-    list_display = ('auth_method','username')
+    list_display = ('auth_method', 'username')
+
 
 class BindHostAdmin(admin.ModelAdmin):
-    list_display = ('host','host_user')
-    list_filter = ('host','host_user')
-    raw_id_fields = ("host",'host_user')
+    list_display = ('host', 'host_user')
+    list_filter = ('host', 'host_user')
+    raw_id_fields = ("host", 'host_user')
 
     # def get_urls(self):
     #
@@ -129,8 +130,7 @@ class BindHostAdmin(admin.ModelAdmin):
     #     #print(my_urls,urls)
     #     return my_urls + urls
 
-
-    #@staff_member_required
+    # @staff_member_required
     def multi_add(self, request):
         if request.user.is_superuser:
             import admin_custom_view
@@ -146,21 +146,22 @@ class BindHostAdmin(admin.ModelAdmin):
                     err = form_obj.err_dic
                 chosen_data = form_obj.clean_data
 
-            #else:
+            # else:
             host_users = models.HostUsers.objects.all()
             hosts = models.Hosts.objects.all()
             host_groups = models.HostGroups.objects.all()
-            return render(request,'admin/web/BindHosts/multi_add.html',{
-                'user':request.user,
-                'host_users':host_users,
-                'host_groups':host_groups,
-                'hosts':hosts,
-                'err':err,
+            return render(request, 'admin/web/BindHosts/multi_add.html', {
+                'user': request.user,
+                'host_users': host_users,
+                'host_groups': host_groups,
+                'hosts': hosts,
+                'err': err,
                 'chosen_data': chosen_data,
                 'result': result
             })
         else:
             return HttpResponse("Only superuser can access this page!")
+
 
 class HostGroupAdmin(admin.ModelAdmin):
     list_display = ('name',)
@@ -168,22 +169,24 @@ class HostGroupAdmin(admin.ModelAdmin):
     #     BindHostInline,
     # ]
     filter_horizontal = ('bind_hosts',)
+
+
 # class UserProfileAdmin(admin.ModelAdmin):
 #     list_display = ('email','name','department','valid_begin_time','valid_end_time')
 #     filter_horizontal = ('host_groups','bind_hosts')
 
 
 class AuditLogAdmin(admin.ModelAdmin):
-    list_display = ('id','session','user','host','action_type','cmd','date')
-    list_filter = ('session','user','host','action_type','date')
-    search_fields = ['user__user__username','host__host__hostname','host__host__ip_addr','cmd']
+    list_display = ('id', 'session', 'user', 'host', 'action_type', 'cmd', 'date')
+    list_filter = ('session', 'user', 'host', 'action_type', 'date')
+    search_fields = ['user__user__username', 'host__host__hostname', 'host__host__ip_addr', 'cmd']
     actions = ['make_published']
-    list_per_page =20
+    list_per_page = 20
     choice_fields = ('action_type',)
-    fk_fields = ('user','host')
+    fk_fields = ('user', 'host')
 
     colored_fields = {
-        'action_type':{'Login':'#83e277','Logout':'orange'}
+        'action_type': {'Login': '#83e277', 'Logout': 'orange'}
     }
 
     def get_actions(self, request):
@@ -195,12 +198,13 @@ class AuditLogAdmin(admin.ModelAdmin):
 
     def make_published(self, request, queryset):
         rows_deleted = models.AuditLog.objects.all()
-        #if rows_updated == 1:
+        # if rows_updated == 1:
         message_bit = "1 story was"
-        #else:
+        # else:
         #    message_bit = "%s stories were" % rows_updated
         self.message_user(request, "%s successfully marked as published." % message_bit)
-    make_published.short_description = '删除3个月以前的审计日志' #
+
+    make_published.short_description = '删除3个月以前的审计日志'  #
 
     def suit_row_attributes(self, obj, request):
         css_class = {
@@ -211,18 +215,21 @@ class AuditLogAdmin(admin.ModelAdmin):
         if css_class:
             return {'class': css_class, 'data': obj.action_type}
 
-
     def has_add_permission(self, request, obj=None):
         return False
 
     def has_delete_permission(self, request, obj=None):
         return False
-    #readonly_fields = models.AuditLog._meta.get_all_field_names()
+    # readonly_fields = models.AuditLog._meta.get_all_field_names()
+
 
 class TaskLogAdmin(admin.ModelAdmin):
-    list_display = ('id','start_time','end_time','task_type','user','cmd','total_task','success_task','failed_task','unknown_task','expire_time')
-    list_filter = ('task_type','user','start_time')
-    #readonly_fields = models.TaskLog._meta.get_all_field_names()
+    list_display = (
+    'id', 'start_time', 'end_time', 'task_type', 'user', 'cmd', 'total_task', 'success_task', 'failed_task',
+    'unknown_task', 'expire_time')
+    list_filter = ('task_type', 'user', 'start_time')
+
+    # readonly_fields = models.TaskLog._meta.get_all_field_names()
     def has_add_permission(self, request, obj=None):
         return False
 
@@ -231,19 +238,25 @@ class TaskLogAdmin(admin.ModelAdmin):
 
     def total_task(self, obj):
         return obj.tasklogdetail_set.select_related().count()
+
     def success_task(self, obj):
         return obj.tasklogdetail_set.select_related().filter(result='success').count()
+
     def failed_task(self, obj):
         return obj.tasklogdetail_set.select_related().filter(result='failed').count()
+
     def unknown_task(self, obj):
         data = "<a href='#'> %s </a> " % obj.tasklogdetail_set.select_related().filter(result='unknown').count()
 
         return data
+
     unknown_task.allow_tags = True
 
+
 class TaskLogDetailAdmin(admin.ModelAdmin):
-    list_display = ('child_of_task','bind_host','result','date')
-    list_filter = ('child_of_task','result','date')
+    list_display = ('child_of_task', 'bind_host', 'result', 'date')
+    list_filter = ('child_of_task', 'result', 'date')
+
     def suit_row_attributes(self, obj, request):
         css_class = {
             'success': 'success',
@@ -252,36 +265,37 @@ class TaskLogDetailAdmin(admin.ModelAdmin):
         }.get(obj.result)
         if css_class:
             return {'class': css_class, 'data': obj.result}
+
     def has_add_permission(self, request, obj=None):
         return False
 
     def has_delete_permission(self, request, obj=None):
         return False
-    #readonly_fields = models.TaskLogDetail._meta.get_all_field_names()
+    # readonly_fields = models.TaskLogDetail._meta.get_all_field_names()
 
 
 class TokenAdmin(admin.ModelAdmin):
-    list_display = ('user','host','token','date','expire')
+    list_display = ('user', 'host', 'token', 'date', 'expire')
+
     def has_add_permission(self, request, obj=None):
         return False
+
     def has_delete_permission(self, request, obj=None):
         return False
-    #readonly_fields = models.Token._meta.get_all_field_names()
+    # readonly_fields = models.Token._meta.get_all_field_names()
 
 
-
-
-admin.site.register(models.Hosts,HostAdmin)
-admin.site.register(models.HostUsers,HostUserAdmin)
-admin.site.register(models.BindHosts,BindHostAdmin)
-admin.site.register(models.HostGroups,HostGroupAdmin)
-admin.site.register(models.UserProfile,UserProfileAdmin)
-#admin.site.register(models.PUserGroups,PUserGroupAdmin)
-admin.site.register(models.AuditLog,AuditLogAdmin)
+admin.site.register(models.Hosts, HostAdmin)
+admin.site.register(models.HostUsers, HostUserAdmin)
+admin.site.register(models.BindHosts, BindHostAdmin)
+admin.site.register(models.HostGroups, HostGroupAdmin)
+admin.site.register(models.UserProfile, UserProfileAdmin)
+# admin.site.register(models.PUserGroups,PUserGroupAdmin)
+admin.site.register(models.AuditLog, AuditLogAdmin)
 admin.site.register(models.TaskLog, TaskLogAdmin)
-#admin.site.register(models.TaskLogDetail)
-admin.site.register(models.TaskLogDetail,TaskLogDetailAdmin)
-admin.site.register(models.Token,TokenAdmin)
+# admin.site.register(models.TaskLogDetail)
+admin.site.register(models.TaskLogDetail, TaskLogDetailAdmin)
+admin.site.register(models.Token, TokenAdmin)
 admin.site.register(models.IDC)
 admin.site.register(models.Department)
 admin.site.register(models.Session)
--- 运维自动化/CrazyEye/web/host_mgr.py
+++ 运维自动化/CrazyEye/web/host_mgr.py
@@ -1,55 +1,50 @@
-#_*_coding:utf-8_*_
-__author__ = 'jieli'
+# _*_coding:utf-8_*_
 
-import json,subprocess
+import json, subprocess
 from web import models
-import paramiko,time,os,signal
+import paramiko, time, os, signal
 import multiprocessing
 from CrazyEye import settings
 from django.db import transaction
 from backend.utils import json_date_handler
 
 
-
-def valid_host_groups_back(request): #deprecated
-    user_groups = models.UserProfile.objects.get(user_id= request.user.id).user_groups.select_related()
+def valid_host_groups_back(request):  # deprecated
+    user_groups = models.UserProfile.objects.get(user_id=request.user.id).user_groups.select_related()
     host_groups = []
 
     for u_group in user_groups:
         host_groups += u_group.host_groups.select_related()
     host_groups = list(set(host_groups))
-    host_group_dic = {-1:[]}
-    selected_g_id  = None
+    host_group_dic = {-1: []}
+    selected_g_id = None
     active_g_item = None
     for h_group in host_groups:
-        hosts = models.BindHosts.objects.filter(host_group_id= h_group.id)
+        hosts = models.BindHosts.objects.filter(host_group_id=h_group.id)
 
-        host_nums = len(set( [i.host.ip_addr for i in hosts ] ))
-        hosts = list(hosts) # convert hosts from models object to list,frontpage needs to loop it
-        selected_group =request.GET.get('selected_group')
+        host_nums = len(set([i.host.ip_addr for i in hosts]))
+        hosts = list(hosts)  # convert hosts from models object to list,frontpage needs to loop it
+        selected_group = request.GET.get('selected_group')
         if selected_group:
             if selected_group.isdigit():
-                if h_group.id == int(selected_group):#got selected
-                    host_group_dic[h_group.id] = [h_group, host_nums,hosts]
+                if h_group.id == int(selected_group):  # got selected
+                    host_group_dic[h_group.id] = [h_group, host_nums, hosts]
                     selected_g_id = h_group.id
                 else:
                     host_group_dic[h_group.id] = [h_group, host_nums]
-            elif selected_group == '-1': #recent visited hosts
+            elif selected_group == '-1':  # recent visited hosts
                 selected_g_id = -1
                 host_group_dic[h_group.id] = [h_group, host_nums]
         else:
             host_group_dic[h_group.id] = [h_group, host_nums]
 
+    return [host_group_dic, selected_g_id]
 
 
-    return [ host_group_dic, selected_g_id]
+def valid_host_groups(request):  # deprecated
+    host_group_dic = {-1: []}
 
-
-
-def valid_host_groups(request): #deprecated
-    host_group_dic = {-1:[]}
-
-    #for group in request.user.host_groups.select_related():
+    # for group in request.user.host_groups.select_related():
 
     '''user_groups = models.UserProfile.objects.get(user_id= request.user.id).user_groups.select_related()
     host_groups = []
@@ -81,38 +76,41 @@ def valid_host_groups(request): #deprecated
             host_group_dic[h_group.id] = [h_group, host_nums]
 
     '''
-    #if not selected_g_id: # first time open host page , set Recent visited hosts as active item
-    #{host_id:[h_group,host_nums, hosts]}
-    return [ host_group_dic, selected_g_id]
+    # if not selected_g_id: # first time open host page , set Recent visited hosts as active item
+    # {host_id:[h_group,host_nums, hosts]}
+    return [host_group_dic, selected_g_id]
 
-def valid_host_list(request): #deprecated
-    user_groups = models.UserProfile.objects.get(user_id= request.user.id).user_groups.select_related()
+
+def valid_host_list(request):  # deprecated
+    user_groups = models.UserProfile.objects.get(user_id=request.user.id).user_groups.select_related()
     host_groups = []
 
     for u_group in user_groups:
         host_groups += u_group.host_groups.select_related()
     host_groups = list(set(host_groups))
-    host_group_dic = {-1:[]}
+    host_group_dic = {-1: []}
 
     for h_group in host_groups:
-        hosts = models.BindHosts.objects.filter(host_group_id= h_group.id)
-        host_nums = len(set( [i.host.ip_addr for i in hosts ] ))
-        hosts = list(hosts) # convert hosts from models object to list,frontpage needs to loop it
+        hosts = models.BindHosts.objects.filter(host_group_id=h_group.id)
+        host_nums = len(set([i.host.ip_addr for i in hosts]))
+        hosts = list(hosts)  # convert hosts from models object to list,frontpage needs to loop it
 
-        host_group_dic[h_group.id] = [h_group, host_nums,hosts]
+        host_group_dic[h_group.id] = [h_group, host_nums, hosts]
 
     return host_group_dic
 
+
 class MultiTask(object):
-    def __init__(self,task_type,request_ins):
+    def __init__(self, task_type, request_ins):
         self.request = request_ins
         self.task_type = task_type
 
     def run(self):
         return self.parse_args()
+
     def parse_args(self):
-        #print '==>parse_args:', self.request.POST
-        task_func = getattr(self,self.task_type)
+        # print '==>parse_args:', self.request.POST
+        task_func = getattr(self, self.task_type)
         return task_func()
 
     def terminate_task(self):
@@ -121,29 +119,29 @@ class MultiTask(object):
         task_obj = models.TaskLog.objects.get(id=int(task_id))
         res_msg = ''
         try:
-            os.killpg(task_obj.task_pid,signal.SIGTERM)
+            os.killpg(task_obj.task_pid, signal.SIGTERM)
             res_msg = 'Task %s has terminated!' % task_id
         except OSError as e:
-            res_msg = "Error happened when tries to terminate task %s , err_msg[%s]" % (task_id,str(e))
-        return  res_msg
+            res_msg = "Error happened when tries to terminate task %s , err_msg[%s]" % (task_id, str(e))
+        return res_msg
 
     def run_cmd(self):
         cmd = self.request.POST.get("cmd")
-        host_ids =[int(i.split('host_')[-1]) for i in json.loads(self.request.POST.get("selected_hosts"))]
+        host_ids = [int(i.split('host_')[-1]) for i in json.loads(self.request.POST.get("selected_hosts"))]
         task_expire_time = self.request.POST.get("expire_time")
         exec_hosts = models.BindHosts.objects.filter(id__in=host_ids)
         random_str = self.request.POST.get('local_file_path')
-        task_obj= self.create_task_log('cmd',exec_hosts,task_expire_time,cmd,random_str)
-        #'-task_type', 'cmd', '-task_id', '15', '-expire', '30', '-task', 'ifconfig', '-uid', '1']
-        print("-->run cmd:",cmd,host_ids)
+        task_obj = self.create_task_log('cmd', exec_hosts, task_expire_time, cmd, random_str)
+        # '-task_type', 'cmd', '-task_id', '15', '-expire', '30', '-task', 'ifconfig', '-uid', '1']
+        print("-->run cmd:", cmd, host_ids)
         p = subprocess.Popen(['python3',
-                             settings.MultiTaskScript,
-                             '-task_type','cmd',
-                             '-expire',task_expire_time,
-                             '-uid',str(self.request.user.id) ,
-                             '-task',cmd ,
-                             '-task_id', str(task_obj.id)
-                             ],
+                              settings.MultiTaskScript,
+                              '-task_type', 'cmd',
+                              '-expire', task_expire_time,
+                              '-uid', str(self.request.user.id),
+                              '-task', cmd,
+                              '-task_id', str(task_obj.id)
+                              ],
                              preexec_fn=os.setsid)
 
         task_obj.task_pid = p.pid
@@ -151,85 +149,83 @@ class MultiTask(object):
         return task_obj.id
 
     @transaction.atomic
-    def create_task_log(self,task_type,hosts,expire_time,content,random_str=None,note=None):
+    def create_task_log(self, task_type, hosts, expire_time, content, random_str=None, note=None):
         task_log_obj = models.TaskLog(
-            task_type = task_type,
-            user = self.request.user,
-            cmd = content,
-            files_dir = random_str,
-            expire_time = int(expire_time),
-            note = note
+            task_type=task_type,
+            user=self.request.user,
+            cmd=content,
+            files_dir=random_str,
+            expire_time=int(expire_time),
+            note=note
         )
         task_log_obj.save()
         task_log_obj.hosts.add(*hosts)
-        #initilize detail logs
+        # initilize detail logs
         for h in hosts:
             task_log_detail_obj = models.TaskLogDetail(
-                child_of_task_id = task_log_obj.id,
-                bind_host_id = h.id,
-                event_log = '',
-                result = 'unknown'
+                child_of_task_id=task_log_obj.id,
+                bind_host_id=h.id,
+                event_log='',
+                result='unknown'
             )
             task_log_detail_obj.save()
 
-        return  task_log_obj
+        return task_log_obj
+
     def file_get(self):
         return self.file_send()
 
     def file_send(self):
         params = json.loads(self.request.POST.get('params'))
-        print("params:",params)
-        host_ids =[int(i.split('host_')[-1]) for i in params.get("selected_hosts")]
+        print("params:", params)
+        host_ids = [int(i.split('host_')[-1]) for i in params.get("selected_hosts")]
         task_expire_time = params.get("expire_time")
         random_str = params.get('local_file_path')
         exec_hosts = models.BindHosts.objects.filter(id__in=host_ids)
         task_type = self.request.POST.get('task_type')
-        #local_file_list = params.get('local_file_list')
+        # local_file_list = params.get('local_file_list')
         if task_type == 'file_send':
-            local_file_list = os.listdir("%s/task_data/tmp/%s" % (settings.FileUploadDir,random_str) )
+            local_file_list = os.listdir("%s/task_data/tmp/%s" % (settings.FileUploadDir, random_str))
 
-            content = "send local files %s to remote path [%s]" %(local_file_list,params.get('remote_file_path'))
+            content = "send local files %s to remote path [%s]" % (local_file_list, params.get('remote_file_path'))
 
         else:
-            local_file_list = 'not_required' #set this var just for passing verification
+            local_file_list = 'not_required'  # set this var just for passing verification
             content = "download remote file [%s]" % params.get('remote_file_path')
 
-
-        task_obj= self.create_task_log(task_type,exec_hosts,task_expire_time,content,random_str)
+        task_obj = self.create_task_log(task_type, exec_hosts, task_expire_time, content, random_str)
         if task_type == 'file_get':
-            local_path = "%s/task_data/%s" %(settings.FileUploadDir,task_obj.id)
+            local_path = "%s/task_data/%s" % (settings.FileUploadDir, task_obj.id)
             try:
-                os.makedirs(local_path,exist_ok=True)
+                os.makedirs(local_path, exist_ok=True)
             except OSError as e:
-                print("err:",e)
-
+                print("err:", e)
 
         p = subprocess.Popen(['python3',
-                             settings.MultiTaskScript,
-                             '-task_type',task_type,
-                             '-expire',task_expire_time,
-                             '-uid',str(self.request.user.id) ,
-                             #'-local',' '.join(local_file_list) ,
-                             '-remote',params.get('remote_file_path') ,
-                             '-task_id', str(task_obj.id)
-                             ],preexec_fn=os.setsid)
+                              settings.MultiTaskScript,
+                              '-task_type', task_type,
+                              '-expire', task_expire_time,
+                              '-uid', str(self.request.user.id),
+                              # '-local',' '.join(local_file_list) ,
+                              '-remote', params.get('remote_file_path'),
+                              '-task_id', str(task_obj.id)
+                              ], preexec_fn=os.setsid)
 
         task_obj.task_pid = p.pid
         task_obj.save()
         return task_obj.id
 
-
-    def get_task_result(self,detail=True):
+    def get_task_result(self, detail=True):
         '''get multi run task result'''
         task_id = self.request.GET.get('task_id')
-        log_dic ={
-            #'summary':{},
-            'detail':{}
+        log_dic = {
+            # 'summary':{},
+            'detail': {}
         }
         task_obj = models.TaskLog.objects.get(id=int(task_id))
         task_detail_obj_list = models.TaskLogDetail.objects.filter(child_of_task_id=task_obj.id)
-        log_dic['summary']={
-            'id':task_obj.id ,
+        log_dic['summary'] = {
+            'id': task_obj.id,
             'start_time': task_obj.start_time,
             'end_time': task_obj.end_time,
             'task_type': task_obj.task_type,
@@ -238,22 +234,22 @@ class MultiTask(object):
             'failed_num': task_detail_obj_list.filter(result='failed').count(),
             'unknown_num': task_detail_obj_list.filter(result='unknown').count(),
             'content': task_obj.cmd,
-            'expire_time':task_obj.expire_time
+            'expire_time': task_obj.expire_time
         }
 
         if detail:
             for log in task_detail_obj_list:
                 log_dic['detail'][log.id] = {
-                    'date': log.date ,
-                    'bind_host_id':log.bind_host_id,
+                    'date': log.date,
+                    'bind_host_id': log.bind_host_id,
                     'host_id': log.bind_host.host.id,
                     'hostname': log.bind_host.host.hostname,
                     'ip_addr': log.bind_host.host.ip_addr,
                     'username': log.bind_host.host_user.username,
-                    'system' : log.bind_host.host.system_type,
+                    'system': log.bind_host.host.system_type,
                     'event_log': log.event_log,
                     'result': log.result,
                     'note': log.note
                 }
 
-        return json.dumps(log_dic,default=json_date_handler)
+        return json.dumps(log_dic, default=json_date_handler)
