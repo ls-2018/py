1883e417195ecb36b53bce4ae08ae5a4493ae574
ls-2018 <38821215+ls-2018@users.noreply.github.com>
2019-05-12 16:20:47 +0800

    CrazyMonitor

--- 运维自动化/CrazyMonitor/monitor/models.py
+++ 运维自动化/CrazyMonitor/monitor/models.py
@@ -17,7 +17,7 @@ class Host(models.Model):
         ('snmp', 'SNMP'),
         ('wget', 'WGET'),
     )
-    monitored_by = models.CharField(u'监控方式', max_length=64, choices=monitored_by_choices)
+    monitored_by = models.CharField('监控方式', max_length=64, choices=monitored_by_choices)
     status_choices = (
         (1, 'Online'),
         (2, 'Down'),
@@ -25,9 +25,9 @@ class Host(models.Model):
         (4, 'Offline'),
         (5, 'Problem'),
     )
-    host_alive_check_interval = models.IntegerField(u"主机存活状态检测间隔", default=30)
-    status = models.IntegerField(u'状态', choices=status_choices, default=1)
-    memo = models.TextField(u"备注", blank=True, null=True)
+    host_alive_check_interval = models.IntegerField("主机存活状态检测间隔", default=30)
+    status = models.IntegerField('状态', choices=status_choices, default=1)
+    memo = models.TextField("备注", blank=True, null=True)
 
     def __str__(self):
         return self.name
@@ -36,7 +36,7 @@ class Host(models.Model):
 class HostGroup(models.Model):
     name = models.CharField(max_length=64, unique=True)
     templates = models.ManyToManyField("Template", blank=True)
-    memo = models.TextField(u"备注", blank=True, null=True)
+    memo = models.TextField("备注", blank=True, null=True)
 
     def __str__(self):
         return self.name
@@ -50,60 +50,37 @@ class ServiceIndex(models.Model):
         ('float', "float"),
         ('str', "string")
     )
-    data_type = models.CharField(u'指标数据类型', max_length=32, choices=data_type_choices, default='int')
-    memo = models.CharField(u"备注", max_length=128, blank=True, null=True)
+    data_type = models.CharField('指标数据类型', max_length=32, choices=data_type_choices, default='int')
+    memo = models.CharField("备注", max_length=128, blank=True, null=True)
 
     def __str__(self):
         return "%s.%s" % (self.name, self.key)
 
 
 class Service(models.Model):
-    name = models.CharField(u'服务名称', max_length=64, unique=True)
-    interval = models.IntegerField(u'监控间隔', default=60)
-    plugin_name = models.CharField(u'插件名', max_length=64, default='n/a')
-    items = models.ManyToManyField('ServiceIndex', verbose_name=u"指标列表", blank=True)
+    name = models.CharField('服务名称', max_length=64, unique=True)
+    interval = models.IntegerField('监控间隔', default=60)
+    plugin_name = models.CharField('插件名', max_length=64, default='n/a')
+    items = models.ManyToManyField('ServiceIndex', verbose_name="指标列表", blank=True)
     has_sub_service = models.BooleanField(default=False,
-                                          help_text=u"如果一个服务还有独立的子服务 ,选择这个,比如 网卡服务有多个独立的子网卡")  # 如果一个服务还有独立的子服务 ,选择这个,比如 网卡服务有多个独立的子网卡
-    memo = models.CharField(u"备注", max_length=128, blank=True, null=True)
+                                          help_text="如果一个服务还有独立的子服务 ,选择这个,比如 网卡服务有多个独立的子网卡")  # 如果一个服务还有独立的子服务 ,选择这个,比如 网卡服务有多个独立的子网卡
+    memo = models.CharField("备注", max_length=128, blank=True, null=True)
 
     def __str__(self):
         return self.name
 
 
 class Template(models.Model):
-    name = models.CharField(u'模版名称', max_length=64, unique=True)
-    services = models.ManyToManyField('Service', verbose_name=u"服务列表")
-    triggers = models.ManyToManyField('Trigger', verbose_name=u"触发器列表", blank=True)
+    name = models.CharField('模版名称', max_length=64, unique=True)
+    services = models.ManyToManyField('Service', verbose_name="服务列表")
+    triggers = models.ManyToManyField('Trigger', verbose_name="触发器列表", blank=True)
 
     def __str__(self):
         return self.name
 
 
-class TriggerExpression(models.Model):
-    trigger = models.ForeignKey('Trigger', verbose_name=u"所属触发器", on_delete=models.CASCADE)
-    service = models.ForeignKey(Service, verbose_name=u"关联服务", on_delete=models.CASCADE)
-    service_index = models.ForeignKey(ServiceIndex, verbose_name=u"关联服务指标", on_delete=models.CASCADE)
-    specified_index_key = models.CharField(verbose_name=u"只监控专门指定的指标key", max_length=64, blank=True, null=True)
-    operator_type_choices = (('eq', '='), ('lt', '<'), ('gt', '>'))
-    operator_type = models.CharField(u"运算符", choices=operator_type_choices, max_length=32)
-    data_calc_type_choices = (
-        ('avg', 'Average'),
-        ('max', 'Max'),
-        ('hit', 'Hit'),
-        ('last', 'Last'),
-    )
-    data_calc_func = models.CharField(u"数据处理方式", choices=data_calc_type_choices, max_length=64)
-    data_calc_args = models.CharField(u"函数传入参数", help_text=u"若是多个参数,则用,号分开,第一个值是时间", max_length=64)
-    threshold = models.IntegerField(u"阈值")
-    logic_type_choices = (('or', 'OR'), ('and', 'AND'))
-    logic_type = models.CharField(u"与一个条件的逻辑关系", choices=logic_type_choices, max_length=32, blank=True, null=True)
-
-    def __str__(self):
-        return "%s %s(%s(%s))" % (self.service_index, self.operator_type, self.data_calc_func, self.data_calc_args)
-
-
 class Trigger(models.Model):
-    name = models.CharField(u'触发器名称', max_length=64)
+    name = models.CharField('触发器名称', max_length=64)
     severity_choices = (
         (1, 'Information'),
         (2, 'Warning'),
@@ -111,13 +88,40 @@ class Trigger(models.Model):
         (4, 'High'),
         (5, 'Diaster'),
     )
-    # expressions = models.ManyToManyField(TriggerExpression,verbose_name=u"条件表达式")
-    severity = models.IntegerField(u'告警级别', choices=severity_choices)
+    # expressions = models.ManyToManyField(TriggerExpression,verbose_name="触发器表达式")
+    severity = models.IntegerField('告警级别', choices=severity_choices)
     enabled = models.BooleanField(default=True)
-    memo = models.TextField(u"备注", blank=True, null=True)
+    memo = models.TextField("备注", blank=True, null=True)
 
     def __str__(self):
         return "<serice:%s, severity:%s>" % (self.name, self.get_severity_display())
+
+
+class TriggerExpression(models.Model):
+    trigger = models.ForeignKey('Trigger', verbose_name="所属触发器", on_delete=models.CASCADE)
+    service = models.ForeignKey(Service, verbose_name="关联服务", on_delete=models.CASCADE)
+    service_index = models.ForeignKey(ServiceIndex, verbose_name="关联服务指标", on_delete=models.CASCADE)
+    specified_index_key = models.CharField(verbose_name="只监控专门指定的指标key", max_length=64, blank=True, null=True)
+    operator_type_choices = (
+        ('eq', '='),
+        ('lt', '<'),
+        ('gt', '>')
+    )
+    operator_type = models.CharField("运算符", choices=operator_type_choices, max_length=32)
+    data_calc_type_choices = (
+        ('avg', 'Average'),
+        ('max', 'Max'),
+        ('hit', 'Hit'),
+        ('last', 'Last'),
+    )
+    data_calc_func = models.CharField("数据处理方式", choices=data_calc_type_choices, max_length=64)
+    data_calc_args = models.CharField("函数传入参数", help_text="若是多个参数,则用,号分开,第一个值是时间", max_length=64)
+    threshold = models.IntegerField("阈值")
+    logic_type_choices = (('or', 'OR'), ('and', 'AND'))
+    logic_type = models.CharField("与一个条件的逻辑关系", choices=logic_type_choices, max_length=32, blank=True, null=True)
+
+    def __str__(self):
+        return "%s %s(%s(%s))" % (self.service_index, self.operator_type, self.data_calc_func, self.data_calc_args)
 
 
 class Action(models.Model):
@@ -125,10 +129,10 @@ class Action(models.Model):
     name = models.CharField(max_length=64, unique=True)
     host_groups = models.ManyToManyField('HostGroup', blank=True)  # 在template里已经关联了主机和tirgger了，为什么这里还要有
     hosts = models.ManyToManyField('Host', blank=True)
-    triggers = models.ManyToManyField('Trigger', blank=True, help_text=u"想让哪些trigger触发当前报警动作")
-    interval = models.IntegerField(u'告警间隔(s)', default=300)
+    triggers = models.ManyToManyField('Trigger', blank=True, help_text="想让哪些trigger触发当前报警动作")
+    interval = models.IntegerField('告警间隔(s)', default=300)
     operations = models.ManyToManyField('ActionOperation')
-    recover_notice = models.BooleanField(u'故障恢复后发送通知消息', default=True)
+    recover_notice = models.BooleanField('故障恢复后发送通知消息', default=True)
     recover_subject = models.CharField(max_length=128, blank=True, null=True)
     recover_message = models.TextField(blank=True, null=True)
 
@@ -141,17 +145,17 @@ class Action(models.Model):
 class ActionOperation(models.Model):
     """报警动作列表"""
     name = models.CharField(max_length=64)
-    step = models.SmallIntegerField(u"第n次告警", default=1, help_text="当trigger触发次数小于这个值时就执行这条记录里报警方式")
+    step = models.SmallIntegerField("第n次告警", default=1, help_text="当trigger触发次数小于这个值时就执行这条记录里报警方式")
     action_type_choices = (
         ('email', 'Email'),
         ('sms', 'SMS'),
         ('script', 'RunScript'),
     )
-    action_type = models.CharField(u"动作类型", choices=action_type_choices, default='email', max_length=64)
-    notifiers = models.ManyToManyField('UserProfile', verbose_name=u"通知对象", blank=True)
+    action_type = models.CharField("动作类型", choices=action_type_choices, default='email', max_length=64)
+    notifiers = models.ManyToManyField('UserProfile', verbose_name="通知对象", blank=True)
     _msg_format = '''Host({hostname},{ip}) service({service_name}) has issue,msg:{msg}'''
 
-    msg_format = models.TextField(u"消息格式", default=_msg_format)
+    msg_format = models.TextField("消息格式", default=_msg_format)
 
     def __str__(self):
         return self.name
@@ -161,7 +165,7 @@ class Maintenance(models.Model):
     name = models.CharField(max_length=64, unique=True)
     hosts = models.ManyToManyField('Host', blank=True)
     host_groups = models.ManyToManyField('HostGroup', blank=True)
-    content = models.TextField(u"维护内容")
+    content = models.TextField("维护内容")
     start_time = models.DateTimeField()
     end_time = models.DateTimeField()
 
