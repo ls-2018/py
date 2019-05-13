bfb96834da1c644bb8794650b264f55ff0ddb079
ls-2018 <38821215+ls-2018@users.noreply.github.com>
2019-05-13 08:05:19 +0800

    CrazyMonitor

--- 运维自动化/CrazyMonitor/monitor/backends/redis_conn.py
+++ 运维自动化/CrazyMonitor/monitor/backends/redis_conn.py
@@ -1,9 +1,10 @@
-#_*_coding:utf-8_*_
+# _*_coding:utf-8_*_
 
 import redis
 
+
 def redis_conn(django_settings):
-    #print(django_settings.REDIS_CONN)
+    # print(django_settings.REDIS_CONN)
     pool = redis.ConnectionPool(host=django_settings.REDIS_CONN['HOST'], port=django_settings.REDIS_CONN['PORT'])
     r = redis.Redis(connection_pool=pool)
-    return  r
+    return r
