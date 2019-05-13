eff4d2ed71eadf264e5b9cfbd225725a3dec5174
ls-2018 <38821215+ls-2018@users.noreply.github.com>
2019-05-13 08:05:01 +0800

    CrazyMonitor

--- 运维自动化/CrazyMonitor/monitor/backends/perpetual_machine.py
+++ 运维自动化/CrazyMonitor/monitor/backends/perpetual_machine.py
@@ -1,24 +1,17 @@
-#_*_coding:utf-8_*_
+# _*_coding:utf-8_*_
 
-import sys,os
+import sys, os
 import django
+
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 sys.path.append(BASE_DIR)
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "CrazyMonitor.settings")
 from CrazyMonitor import settings
 
 django.setup()
-from  monitor import models
+from monitor import models
 from monitor.backends import data_processing
-
-
 
 if __name__ == '__main__':
     reactor = data_processing.DataHandler(settings)
     reactor.looping()
-
-
-
-
-
-
