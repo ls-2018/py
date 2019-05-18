d7a9c3b3c0bac7c5b6d1417ed3ea155275cae5a1
ls-2018 <38821215+ls-2018@users.noreply.github.com>
2019-05-18 08:10:32 +0800

    面试

--- Python程序员面试算法宝典/面试/518.py
+++ Python程序员面试算法宝典/面试/518.py
@@ -48,3 +48,64 @@ f(2)
 f(3, [3, 2, 1])
 f(3)
 print('----------------------')
+
+
+class A(object):
+    def go(self):
+        print("go A go!")
+
+    def stop(self):
+        print("stop A stop!")
+
+    def pause(self):
+        raise Exception("Not Implemented")
+
+
+class B(A):
+    def go(self):
+        super(B, self).go()
+        print("go B go!")
+
+
+class C(A):
+    def go(self):
+        super(C, self).go()
+        print("go C go!")
+
+    def stop(self):
+        super(C, self).stop()
+        print("stop C stop!")
+
+
+class D(B, C):
+    def go(self):
+        super(D, self).go()
+        print("go D go!")
+
+    def stop(self):
+        super(D, self).stop()
+        print("stop D stop!")
+
+    def pause(self):
+        print("wait D wait!")
+
+
+class E(B, C):
+    pass
+
+
+a = A()
+b = B()
+c = C()
+d = D()
+e = E()
+a.stop()
+b.stop()
+c.stop()
+d.stop()
+e.stop()
+a.pause()
+b.pause()
+c.pause()
+d.pause()
+e.pause()
