739f81060e403886ca9d28fa2d575343df5e417d
ls-2018 <38821215+ls-2018@users.noreply.github.com>
2019-05-18 12:00:38 +0800

    面试

--- Python程序员面试算法宝典/面试/520.py
+++ Python程序员面试算法宝典/面试/520.py
@@ -192,3 +192,26 @@
 #     pass
 # import functools
 # functools.partial
+def swap(str, i, j):
+    tmp = str[i]
+    str[i] = str[j]
+    str[j] = tmp
+
+
+def permutation(str, start):
+    if str is None or start < 0:
+        return
+    if start == len(str) - 1:
+        print(''.join(str))
+    else:
+        i = start
+        while i < len(str):
+            swap(str, start, i)
+            permutation(str, start + 1)
+            swap(str, start, i)
+            i += 1
+
+
+if __name__ == '__main__':
+    s = list('aabc')
+    permutation(s, 0)
