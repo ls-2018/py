f9bc583172545ed0c66ab934365f01592853a916
ls-2018 <38821215+ls-2018@users.noreply.github.com>
2019-06-16 15:30:11 +0800

    sanic配置

--- sanic框架/2、加载配置.py
+++ sanic框架/2、加载配置.py
@@ -2,17 +2,37 @@ from sanic import Sanic
 
 from sanic.response import json
 
-app = Sanic()
+app = Sanic(load_env=True)
 # app.config.DB_HOST=''
 app.config.update({
     'DB_HOST': "localhost",
     'DB_NAME': "online",
     'DB_USER': 'root'
 })
+# 1、使用环境变量，前缀必须是  SANIC_
+"""
+例如环境变量SANIC_REQUEST_TIMEOUT在程序中自动加载并将其输入到REQUEST_TIMEOUT配置变量中，
+前缀也可以自己设置，app = Sanic(load_env="MYAPP_")
+"""
 
+
+# 2、来自对象
+class A:
+    REQUEST_TIMEOUT = 121
+
+
+app.config.from_object(A())
+
+# 3、来自文件
+app.config.from_pyfile('123.txt')
+"""
+从环境变量中指定配置文件的位置，并告诉sanic使用它来查找配置文件
+app.config.from_envvar('myapp_settings')
+"""
 
 @app.route('/')
 async def test(request):
+    print(app.config.get('REQUEST_TIMEOUT', '-----------'), '  -----------')
     return json({'json': "hello"})
 
 
