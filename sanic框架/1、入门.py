05ecde7a6fc814b4eb952347135c395c8961b3a8
ls-2018 <38821215+ls-2018@users.noreply.github.com>
2019-06-16 15:06:13 +0800

    sanic

--- sanic框架/1、入门.py
+++ sanic框架/1、入门.py
@@ -4,9 +4,11 @@ from sanic.response import json
 
 app = Sanic()
 
+
 @app.route('/')
 async def test(request):
-    return json({'json':"hello"})
+    return json({'json': "hello"})
+
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0',port=8000)
+    app.run(host='0.0.0.0', port=8000)
