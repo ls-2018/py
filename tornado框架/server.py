3329c8f8a8ce552983e9534d76cc480b1f79ce9f
ls-2018 <38821215+ls-2018@users.noreply.github.com>
2019-06-22 20:53:10 +0800

    tornado

--- tornado框架/server.py
+++ tornado框架/server.py
@@ -1,6 +1,8 @@
 import tornado.web
 # 基础web框架模块
 import tornado.ioloop
+
+
 # 核心IO循环模块，封装了Linux的epoll和BSD的kqueue
 
 # 类比django中的视图类
@@ -14,5 +16,14 @@ if __name__ == '__main__':
     app = tornado.web.Application([
         ('/', IndexHandler)
     ])
-    app.listen(8000)
-    tornado.ioloop.IOLoop.current().start()
+    # app.listen(8000)  ---->  返回了一个服务器
+    # 手动创建一个服务器
+    import tornado.httpserver
+
+    server = tornado.httpserver.HTTPServer(app)
+    server.listen(8088, '127.0.0.1')
+
+    # tornado.ioloop.IOLoop.current().start()
+    """
+    IOLoop.current()    返回当前线程的IOLoop实例
+    """
