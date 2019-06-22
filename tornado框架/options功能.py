0f8919885a8242be6a22310e4bd3ad2bc7ae5922
ls-2018 <38821215+ls-2018@users.noreply.github.com>
2019-06-22 22:17:53 +0800

    tornado

---
+++ tornado框架/options功能.py
@@ -0,0 +1,20 @@
+import tornado.options
+"""
+作用：     
+    全局参数的定义、存储、转换
+    
+"""
+tornado.options.define(name, default=None, type=None, help=None, metavar=None,
+           multiple=False, group=None, callback=None)
+"""
+name
+default=None
+type=None
+help=None
+metavar=None
+multiple=False
+group=None
+callback=None
+
+
+"""
