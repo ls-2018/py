cbde5830b48c292c04beb68fabafd2acf09e8247
ls-2018 <38821215+ls-2018@users.noreply.github.com>
2019-06-22 22:23:46 +0800

    tornado

--- tornado框架/options功能.py
+++ tornado框架/options功能.py
@@ -1,20 +1,26 @@
 import tornado.options
+import tornado
 """
 作用：     
     全局参数的定义、存储、转换
     
 """
-tornado.options.define(name, default=None, type=None, help=None, metavar=None,
+tornado.options.define('name', default=None, type=None, help=None, metavar=None,
            multiple=False, group=None, callback=None)
+
+print(tornado.options.options.as_dict())
 """
-name
+name                    选项变量名,(唯一)
 default=None
-type=None
+type=None               选项类型(int,str,...),从命令行或配置文件导入参数时tornado会类型转换
 help=None
 metavar=None
 multiple=False
 group=None
 callback=None
 
+
+
+用来定义options选项变量的方法
 
 """
