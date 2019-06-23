b3c62c8458de1e2c7413be3844b2630ae60106c7
ls-2018 <38821215+ls-2018@users.noreply.github.com>
2019-06-23 19:07:06 +0800

    tornado规划

--- tornado框架/project/config.py
+++ tornado框架/project/config.py
@@ -3,13 +3,21 @@ import os
 BASE_DIR = os.path.dirname(__file__)
 # 启动参数
 options = {
-    'port': 8000
+    'port': 8001
 }
 
 # 系统配置
 settings = {
     "debug": True,
     'static_path': os.path.join(BASE_DIR, 'static'),
-    'static_url_prefix': None,
+    'static_url_prefix': '/static',
     'template_path': os.path.join(BASE_DIR, 'templates')
 }
+
+"""
+debug=True
+    1、自动重启                        autoreload=True
+    2、取消缓存编译的模板              compiled_template_cache=False
+    3、取消缓存静态文件的hash值        static_hash_cache=False
+    4、提供追踪信息                    serve_traceback=True
+"""
--- tornado框架/project/server.py
+++ tornado框架/project/server.py
@@ -2,6 +2,7 @@ import tornado.ioloop
 from config import options
 from urls import Application
 
+
 if __name__ == '__main__':
     app = Application()
     app.listen(options['port'])
