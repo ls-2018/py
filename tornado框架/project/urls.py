1025f0860cfd96dc0bb6a35dc519d16c565f179b
ls-2018 <38821215+ls-2018@users.noreply.github.com>
2019-06-23 19:42:13 +0800

    tornado规划

--- tornado框架/project/urls.py
+++ tornado框架/project/urls.py
@@ -6,7 +6,7 @@ from config import settings
 class Application(tornado.web.Application):
     def __init__(self, *args, **kwargs):
         handlers = [
-            ('/', index.IndexHandler)
+            ('/', index.IndexHandler, {'name': 'zhangsan'})
 
         ]
         super(Application, self).__init__(handlers, *args, **kwargs, **settings)
--- tornado框架/project/views/index.py
+++ tornado框架/project/views/index.py
@@ -2,5 +2,12 @@ from tornado.web import RequestHandler
 
 
 class IndexHandler(RequestHandler):
+    def initialize(self, **kwargs):
+        self.recv = kwargs
+
     def get(self, *args, **kwargs):
         self.write('<h1>hello world!</h1>')
+        self.write('<h1>hello world!</h1>')
+        self.write('<h1>hello world!</h1>')
+        self.finish()
+        print(self.recv)
