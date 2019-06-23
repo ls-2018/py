19336fd4a823ba3b9f0ab01d4b88e55670595594
ls-2018 <38821215+ls-2018@users.noreply.github.com>
2019-06-23 11:17:44 +0800

    tornado

--- tornado框架/options功能.py
+++ tornado框架/options功能.py
@@ -1,26 +1,38 @@
 import tornado.options
 import tornado
+
 """
 作用：     
     全局参数的定义、存储、转换
     
 """
-tornado.options.define('name', default=None, type=None, help=None, metavar=None,
-           multiple=False, group=None, callback=None)
 
-print(tornado.options.options.as_dict())
+tornado.options.define('list', default=[], type=str, help='this is a port info', metavar=None,
+                       multiple=True, group=None, callback=None)
 """
 name                    选项变量名,(唯一)
 default=None
-type=None               选项类型(int,str,...),从命令行或配置文件导入参数时tornado会类型转换
-help=None
+type=None               选项类型(int,float,str,datetime,timedelta),从命令行或配置文件导入参数时tornado会类型转换
+                            如果没有设置type，会根据default的值进行转换
+                            如果default没有设置，那么不进行转换
+help=None               帮助信息
 metavar=None
-multiple=False
+multiple=False          设置选项是否可以为多个值,默认为False,
 group=None
 callback=None
-
-
-
 用来定义options选项变量的方法
 
 """
+
+# -------------------------------------------------从命令行-------------------------------------------------
+
+# # 获取参数的方法
+# tornado.options.parse_command_line()  # 命令行解析
+# # python options功能.py --list=123456,456
+#
+# print(tornado.options.options.as_dict())
+# print(tornado.options.options.list)  # ['123456', '456']
+
+# -------------------------------------------------从配置文件-------------------------------------------------
+tornado.options.parse_config_file('./conf')
+print(tornado.options.options.as_dict())
