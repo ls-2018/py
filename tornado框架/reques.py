e8e16809461f4ec180b5c944f07ff695108d103f
ls-2018 <38821215+ls-2018@users.noreply.github.com>
2019-06-25 00:09:02 +0800

    tornado规划

--- tornado框架/reques.py
+++ tornado框架/reques.py
@@ -11,6 +11,7 @@
     (r'/liu/(\w+)/(\w+)/')              def get(self,h1,h2,*args,**kwargs)
     (r'/liu/(?P<P2>\w+)/(?P<P1>\w+)/')              def get(self,P1,P2,*args,**kwargs)
     get方式传递参数
+    self.get_query_argument('flag'，default=_ARG_DEFAULT, strip=True) # 没有报错
     post方式传递参数
     既可以获取get请求，也可以获取post请求
     在http报文的头中增加自定义的字段
