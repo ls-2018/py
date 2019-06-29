6a5f1fc4fd3047782043893d691ac79987c69d51
ls-2018 <38821215+ls-2018@users.noreply.github.com>
2019-06-29 13:59:59 +0800

    转义

--- tornado框架/project/urls.py
+++ tornado框架/project/urls.py
@@ -1,13 +1,17 @@
 import tornado.web
 from views import index
 from config import settings
+from tornado.web import StaticFileHandler
+import os
 
 
 class Application(tornado.web.Application):
     def __init__(self, *args, **kwargs):
         handlers = [
             ('/', index.IndexHandler, {'name': 'zhangsan'}),
-            tornado.web.url('/kind', index.KindHandler, kwargs={'arg': "demo"}, name='kind')
+            tornado.web.url('/kind', index.KindHandler, kwargs={'arg': "demo"}, name='kind'),
+            ('/(.*)$', StaticFileHandler,
+             {'path': os.path.join(settings.BASE_DIR, 'static'), 'default_filename': "index.html"})
 
         ]
         super(Application, self).__init__(handlers, *args, **kwargs, **settings)
---
+++ tornado框架/静态文件.py
@@ -0,0 +1,9 @@
+"""
+@Time: 2019/6/28 17:03 
+@Author: liushuo
+@File: 静态文件.py 
+@Desc: 
+@Software: PyCharm
+"""
+static_path=''
+from tornado.web import StaticFileHandler
