b2104c86ebe4f553974010dbdfb47a56de721401
ls-2018 <38821215+ls-2018@users.noreply.github.com>
2019-07-01 16:11:41 +0800

    aws

---
+++ AWS/__init__.py
@@ -0,0 +1,20 @@
+"""
+@Time: 2019/7/1 13:38 
+@Author: liushuo
+@File: __init__.py 
+@Desc: 
+@Software: PyCharm
+"""
+# import boto3
+#
+# s3 = boto3.resource('s3')  # choice service
+#
+# # Print out bucket names
+# for bucket in s3.buckets.all():  # bucket  name
+#     print(bucket.name)
+#
+# # Upload a new file
+# data = open('test.jpg', 'rb')
+# # s3.Bucket('my-bucket').put_object(Key='test.png', Body=data)
+# # 要先创建库
+# print(s3.Bucket('my-bucket').get_object(Key='test.png'))
