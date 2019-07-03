aa4f5d5ecc1cd169e9d9a3a4c6941c961410b775
ls-2018 <38821215+ls-2018@users.noreply.github.com>
2019-07-03 12:56:46 +0800

    C

--- C/C_test/data.c
+++ C/C_test/data.c
@@ -3,7 +3,7 @@
 //
 # include"123.h"
 #include <stdio.h>
-
+int a,b,c;
 void add() {
     a = 3;
     b = 2;
--- C/C_test/main.c
+++ C/C_test/main.c
@@ -1,10 +1,8 @@
 #include <stdio.h>
-#include "data.c"
+#include "data.h"
 #include "123.h"
 // 同一个作用域，变量不能定义两次
 void main() {
-    int a = 1;
-
     add();
 //    return 0;
 }
