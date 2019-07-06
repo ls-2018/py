b05029a9171d1af17486632b17bc683ae4608567
ls-2018 <38821215+ls-2018@users.noreply.github.com>
2019-07-06 12:43:23 +0800

    C

---
+++ C/4、基本运算符与表达式/4.11 put-get-char.c
@@ -0,0 +1,15 @@
+#include<stdio.h> // 数据类型的极限值
+#include<stdlib.h>
+#include<math.h>
+#include<limits.h>
+
+void main()
+{
+	putchar(65);
+	putchar('a');
+	putchar('\n');
+
+	puts("xxx");// 会在末尾自动插入换行
+
+	getchar();
+}
