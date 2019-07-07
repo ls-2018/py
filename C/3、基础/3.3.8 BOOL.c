21a511c382e37bf4c4b50549a97a794b3d367b33
ls-2018 <38821215+ls-2018@users.noreply.github.com>
2019-07-07 14:13:36 +0800

    C

--- C/3、基础/3.3.8 BOOL.c
+++ C/3、基础/3.3.8 BOOL.c
@@ -1,10 +1,11 @@
 #include<stdio.h> // 数据类型的极限值
-#include<stdbool.h> // 支持c语言的bool型变量
+//#include<stdbool.h> // 支持c语言的bool型变量
 
 
 void main(){
-	bool bl;
-	bl = false;
+	_Bool bl;
+	//bool bl;
+	bl = 0;
 	printf("%d\n", bl);
 	printf("%d\n", sizeof(bl));
 	// 成立为1，占一个字节
--- C/谭浩强/第3章/课后题/1、国民生产总值.c
+++ C/谭浩强/第3章/课后题/1、国民生产总值.c
@@ -2,7 +2,12 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<math.h>
-void main(){
-
+void main()
+{
+    float p, r, n;
+    r = 0.07;
+    n = 10;
+    p = pow (1 + r, n);
+    printf ("%f\n", p);
     system ("pause");
 }
