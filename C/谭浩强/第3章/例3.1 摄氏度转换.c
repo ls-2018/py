56ada905ed1ca9866f9f1bddbc327208f9cd2539
ls-2018 <38821215+ls-2018@users.noreply.github.com>
2019-07-07 13:28:27 +0800

    C

---
+++ C/谭浩强/第3章/例3.1 摄氏度转换.c
@@ -0,0 +1,12 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<stdlib.h>
+
+void main()
+{
+    float f, c;
+    f = 64;
+    c = (5.0 / 9) * (f - 32);
+    printf ("%f,%f", f, c);
+    system ("pause");
+}
---
+++ C/谭浩强/第3章/例3.3 大转小.c
@@ -0,0 +1,13 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<stdlib.h>
+
+void main()
+{
+    char c1, c2;
+    c1 = 'A';
+    c2 = c1 + 32;
+    printf ("%c\n", c1);
+    printf ("%c\n", c2);
+    system ("pause");
+}
