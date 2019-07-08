da94827eafca2ef08182b50e0fd6d01721d90913
ls-2018 <38821215+ls-2018@users.noreply.github.com>
2019-07-08 22:38:28 +0800

    C

--- C/6、数组/5、杨辉三角.c
+++ C/6、数组/5、杨辉三角.c
@@ -9,32 +9,35 @@ void main()
 
     for (int i = 0; i < N; i++)
     {
-
         for (int j = 0; j < i; j++)
         {
             if (j == 0 || i == j)
             {
                 a[i][j] = 1;
             }
+
             else
             {
                 a[i][j] = a[i - 1][j - 1] + a[i - 1][j];
             }
-			printf("%4d", a[i][j]);
+
+            printf ("%4d", a[i][j]);
         }
 
         printf ("\n");
     }
 
-	for (int i = 0; i < N; i++)
-	{
-		printf("%*d", 30 - i * 2, a[i][0]); //打印第一列
-		for (int j = 1; j < i; j++)
-		{
-			printf("%4d", a[i][j]);
-		}
+    for (int i = 0; i < N; i++)
+    {
+        printf ("%*d", 30 - i * 2, a[i][0]); //打印第一列
 
-		printf("\n");
-	}
+        for (int j = 1; j < i; j++)
+        {
+            printf ("%4d", a[i][j]);
+        }
+
+        printf ("\n");
+    }
+
     getchar();
 }
