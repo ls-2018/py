ec6726753bec4b8e71d56fa87986e466b1fb4aa3
ls-2018 <38821215+ls-2018@users.noreply.github.com>
2019-07-09 23:55:58 +0800

    C

--- C/6、数组/10、多元数组.cpp
+++ C/6、数组/10、多元数组.cpp
@@ -12,6 +12,11 @@ void main()
     auto data1 = std::get<1> (mytuple);
     auto data2 = std::get<2> (mytuple);
     auto data3 = std::get<3> (mytuple);
+
+    std::cout<<typeid(data3).namespace()<<std::endl;
+    decltype(data0) dataA;// 获取数据类型再次创建
+    // mytuple.swap(mytuple);       array.vetor 都有交换的功能
+
     std::cout << "data0" << data0;
     getchar();
 }
