76d7936d6841a6cfce9a1f42840fd1f5672da5b9
ls-2018 <38821215+ls-2018@users.noreply.github.com>
2019-07-11 22:45:44 +0800

    C

---
+++ C/7、指针/6、重要-函数返回值指针.c
@@ -0,0 +1,32 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<string.h>
+
+char * mystrcpy (char * dest, char * source)
+{
+    char *last = NULL;// 最后结果
+
+    if (dest == NULL || source == NULL)
+    {
+        return last;
+    }
+
+    last = dest;
+
+    //while ((*dest++ = *source++) != '\0');
+    while (*source != '\0')
+    {
+        /**dest = *source;
+        dest++;
+        source++;*/
+        *dest++ = *source++; // a=b整体表达式的值，就是b的值
+    }
+}
+
+
+void main()
+{
+    char str[40] = {0};
+    printf ("%s\n", mystrcpy (str, "hello world!"));
+    getchar();
+}
