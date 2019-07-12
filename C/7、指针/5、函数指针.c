faac8c03679c545e3f978a9694ba5393f384ec88
ls-2018 <38821215+ls-2018@users.noreply.github.com>
2019-07-12 10:25:14 +0800

    C

--- C/7、指针/5、函数指针.c
+++ C/7、指针/5、函数指针.c
@@ -1,14 +1,14 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include<stdio.h>
 #include<string.h>
-void msg()
+void msg(int x)
 {
     printf ("---------\n");
 }
 
 void main()
 {
-    void (*p) () = msg;
+    void (*p) (int x) = msg;
     // º¯ÊýÖ¸Õë
     p();
     getchar();
