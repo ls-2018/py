c6cf8fc08969eff416e0fa7bff071d00bfe3cef2
ls-2018 <38821215+ls-2018@users.noreply.github.com>
2019-07-14 20:21:31 +0800

    AWS

--- C/8、字符串/1、声明.c
+++ C/8、字符串/1、声明.c
@@ -9,7 +9,7 @@ void main()
     srand ( (unsigned int) time (&ts));
     int num = rand() % 10;
     //字符串和\0来存储字符串
-    char *str1 = "hello";	// 字符常量，可以用字符串进行初始化；{}可以省略
+    char *str1 = "hello";	//str1存储内存地址 字符常量，可以用字符串进行初始化；{}可以省略
     //char str[5] = { 'c', 'a', 'l', 'c', '\0' }; // 字符数组可以修改
     char str[5] = "calc";
     printf ("%x	,%x \n", sizeof (str), sizeof ("calc"));	// 5	,5
