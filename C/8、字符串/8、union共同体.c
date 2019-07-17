c27d1f6df5bb8eeb85e4129c5e7aa1ed3c97114c
ls-2018 <38821215+ls-2018@users.noreply.github.com>
2019-07-17 23:24:58 +0800

    c

--- C/8、字符串/8、union共同体.c
+++ C/8、字符串/8、union共同体.c
@@ -6,6 +6,10 @@
 	共同体是几个不同类型的变量共占一段内存，相互覆盖；
 在任何时刻只有一个成员存在；
 共同体变量定义分配内存，长度=最长成员所占字节数
+
+不能引用共同体变量，只能引用其成员
+共同体变量中起作用的成员是最后一次存放的成员
+不能在变量初始化只能{first}对第一个变量进行赋值
 */
 union info
 {
@@ -19,12 +23,17 @@ union
     char addr[100];
     int num;
 } d8;
-
+union  ddf	// 16
+{
+    double b;	// 8
+    char str[9];	// 9
+} ff;// 共同体的大小，一定可以被最小类型所整除
 void main()
 {
     union info d1;
     strcpy (d1.addr, "world");
-    printf ("%d\n", sizeof (d1)); // 最大成员变量所占的内存大小
-    printf ("%d	%s\n", d1.num, d1.addr);
+    //printf("%d\n", sizeof(d1)); // 最大成员变量所占的内存大小
+    printf ("%d\n", sizeof (union  ddf)); // 最大成员变量所占的内存大小
+    //printf ("%d	%s\n", d1.num, d1.addr);
     system ("pause");
 }
