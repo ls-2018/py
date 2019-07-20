caf9fb78aaf8374e54bd302922076b1085ca1bed
ls-2018 <38821215+ls-2018@users.noreply.github.com>
2019-07-20 19:21:21 +0800

    c

---
+++ C/10、位运算/4、位移显示补码.c
@@ -0,0 +1,30 @@
+#define   _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+
+void main()
+{
+    int num;
+    scanf ("%d", &num);
+    int data = 1 << 31;
+
+    for (int i = 0; i <= 31; i++)
+    {
+        printf ("%c", (num & (data >> i)) ? '1' : '0');
+
+        if ( ( (i + 1) % 4) == 0)
+        {
+            printf (" ");
+        }
+    }
+
+    {
+        /*	int *pint;
+        	pint = (int *)malloc(sizeof(struct bits) * 4);
+        	scanf("%d",pint)*/
+    }
+
+    system ("pause");
+}
