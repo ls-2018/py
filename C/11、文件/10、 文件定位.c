58a1eb7505f901f190b8b14994efa7e961ab6bf6
ls-2018 <38821215+ls-2018@users.noreply.github.com>
2019-07-24 21:49:13 +0800

    c

--- C/11、文件/10、 文件定位.c
+++ C/11、文件/10、 文件定位.c
@@ -1,2 +1,11 @@
 rewind(fp);
-ftell(fp);执行成功时，返回当前文件指针到文件头有多少个字节，否则，返回-1
+int size = ftell(fp);执行成功时，返回当前文件指针到文件头有多少个字节，否则，返回-1
+
+
+
+int num = fseek(fp,偏移，起始)
+
+起始点     值   名称
+文件开始    0   SEEK_SET
+文件末尾    2   SEEK_END
+当前位置    1   SEEK_CUR
