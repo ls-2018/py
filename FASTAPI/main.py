3b6ea91ad3ac95c7bd336bd82af26e40cb955fc5
liushuo-at-907457203140 <1214972346@qq.com>
2020-05-23 17:38:44 +0800

    fastapi request

--- FASTAPI/main.py
+++ FASTAPI/main.py
@@ -1,13 +1,20 @@
 from enum import Enum
 from typing import List, Dict, Set, Tuple, Optional
-from fastapi import FastAPI, Query, Path, Body, Cookie
+from fastapi import FastAPI, Query, Path, Body, Form, File, UploadFile, Cookie, Header
 from pydantic import BaseModel, Field, HttpUrl
 from uuid import UUID
+from starlette.requests import Request
+from starlette.templating import Jinja2Templates
+from starlette.staticfiles import StaticFiles
 from datetime import datetime, time, timedelta
 import uvicorn
 
 app = FastAPI()
+templates = Jinja2Templates(directory='templates')
+app.mount('/static', StaticFiles(directory='static'), name='static')
 
+
+# {{url_for("static",path="/image/fav.icon")}}
 
 class Item(BaseModel):
     # 有等于号的是可选参数
@@ -73,9 +80,10 @@ async def read_item(
 @app.put("/items/{item_id}")
 async def update_item(
         item_id: UUID,
-        repeat_at: time = Body(None),
-        item: Item = Body(..., embed=False)
-):  # True将item在包装一层{item：{}}
+        repeat_at: time = Body(None),  # 不再是请求参数;而是请求体参数
+        item: Item = Body(..., embed=False),  # True将item在包装一层{item：{}}
+):
+    print(item.dict())
     return {
         "item_name": item.name,
         "repeat_at": repeat_at,
@@ -83,6 +91,30 @@ async def update_item(
     }
 
 
+@app.get('/render')
+async def render(request: Request):
+    return templates.TemplateResponse('index.html', {'request': request})
+
+
+@app.post('/files/')
+def user(request: Request, username: str = Form(..., media_type="application/x-www-form-urlencoded"),
+         file_list: List[bytes] = File(...),
+         file_name: List[UploadFile] = File(...)
+         ):
+    for item in file_list:
+        print(len(item))
+    for item in file_name:
+        print(item.filename)
+    return {"ok": 1}
+
+
+@app.get("/header")
+def header(request: Request, user_agent: str = Header(None)):
+    # user_agent:str=Header(None) 匹配 User-Agent
+    print(user_agent)
+    return user_agent
+
+
 if __name__ == '__main__':
-    uvicorn.run("main:app", host='127.0.0.1', port=8000, log_level='info')
+    uvicorn.run("main:app", host='127.0.0.1', port=8000, log_level='info', reload=True)
 # uvicorn main:app --reload
