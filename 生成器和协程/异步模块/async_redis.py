e94269910c1d82b277abb10a6570921634ba166d
liushuo-at-907457203140 <1214972346@qq.com>
2020-05-23 12:33:08 +0800

    异步模块

--- 生成器和协程/异步模块/async_redis.py
+++ 生成器和协程/异步模块/async_redis.py
@@ -4,6 +4,7 @@ pip install aioredis
 """
 
 import asyncio
+from aioredis import Redis
 import aioredis
 
 
@@ -16,6 +17,18 @@ async def execute(address, password):
 
     redis.close()
     await redis.wait_closed()
+    print('end')
+
+
+async def execute2(address, password):
+    pool = await aioredis.ConnectionsPool(address, password=password, minsize=1, maxsize=10)
+    conn = await pool.acquire()
+    redis = Redis(conn)
+    await redis.hmset_dict("car", key1=1, key2=2, key3=3)
+    result = await redis.hgetall('car', encoding='utf8')
+    print(result)
+
+    pool.release(conn)
     print('end')
 
 
