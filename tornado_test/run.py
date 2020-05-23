7ebde9f8fca2f320dce4954a256198b4b8e18cc6
liushuo-at-907457203140 <1214972346@qq.com>
2020-05-23 12:04:18 +0800

    -

--- tornado_test/run.py
+++ tornado_test/run.py
@@ -4,10 +4,10 @@ from app.utils.command import init_table
 
 
 class Program:
-    # @staticmethod
-    # def run(self):
-    #     """run this program"""
-    #     Application().start_server()
+    @staticmethod
+    def run(self):
+        """run this program"""
+        Application().start_server()
 
     def migrate(self, drop=False):
         """
